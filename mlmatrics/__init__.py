ed8393c0db16e0d90334fe94eaf9d607894b2bbe
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-02-24 17:37:30 +0100

    add residual_vs_actual(), residual_hist() in new histograms.py, add readme glossary

--- mlmatrics/__init__.py
+++ mlmatrics/__init__.py
@@ -4,15 +4,17 @@ from .elements import (
     hist_elemental_prevalence,
     ptable_elemental_prevalence,
 )
+from .histograms import residual_hist
 from .metrics import regression_metrics
 from .parity import (
     density_hexbin,
     density_hexbin_with_hist,
     density_scatter,
     density_scatter_with_hist,
-    err_scatter,
+    residual_vs_actual,
+    scatter_with_err_bar,
 )
 from .quantile import qq_gaussian
 from .ranking import err_decay
 from .relevance import precision_recall_curve, roc_curve
-from .utils import ROOT
+from .utils import ROOT, add_identity, show_bar_values
