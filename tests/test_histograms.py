41297ec50cfd16ef2edb908b5e7921bae6916365
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-03-24 14:38:16 +0100

    add spacegroup_hist to histograms.py

--- tests/test_histograms.py
+++ tests/test_histograms.py
@@ -1,4 +1,4 @@
-from mlmatrics import residual_hist, true_pred_hist
+from mlmatrics import residual_hist, spacegroup_hist, true_pred_hist
 
 from . import y_pred, y_true
 
@@ -9,3 +9,7 @@ def test_residual_hist():
 
 def test_true_pred_hist():
     true_pred_hist(y_true, y_pred, y_true - y_pred)
+
+
+def test_spacegroup_hist():
+    spacegroup_hist(range(1, 231))
