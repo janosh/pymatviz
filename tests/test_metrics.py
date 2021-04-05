7b8b610d66a92f42517d85fad02661cf33890372
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-04-05 11:01:58 +0200

    manually merge branch metrics. thx @CompRhys!

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-from mlmatrics import regression_metrics
+from mlmatrics import classification_metrics, regression_metrics
 
-from . import y_pred, y_true
+from . import y_binary, y_pred, y_proba, y_true
 
 
 def test_regression_metrics():
@@ -16,5 +16,23 @@ def test_regression_metrics_ensemble():
     # simulate 2-model ensemble by duplicating predictions along 0-axis
     y_preds = np.tile(y_pred, (2, 1))
     metrics = regression_metrics(y_true, y_preds, verbose=False)
+    assert metrics["single"]
+    assert metrics["ensemble"]
+
+
+def test_classification_metrics():
+    y_probs = np.expand_dims(y_proba, axis=(0, 2))
+    metrics = classification_metrics(y_binary, y_probs, verbose=False)
+    assert metrics["acc"]
+    assert metrics["roc_auc"]
+    assert metrics["precision"]
+    assert metrics["recall"]
+    assert metrics["f1"]
+
+
+def test_classification_metrics_ensemble():
+    y_probs = np.expand_dims(y_proba, axis=(0, 2))
+    y_probs = np.tile(y_probs, (2, 1, 1))
+    metrics = classification_metrics(y_binary, y_probs, verbose=False)
     assert metrics["single"]
     assert metrics["ensemble"]
