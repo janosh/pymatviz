9512d4e4041eda1b01c19c8d87e457973a21e870
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-10-15 15:21:49 +0100

    refactor ptable_heatmap to plot arbitrary data, not just elemental prevalence, add element properties to elements.csv

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -8,59 +8,57 @@ from ml_matrics import (
 )
 
 
-compositions_a = pd.read_csv("data/mp-n_elements<2.csv").formula
-compositions_b = pd.read_csv("data/ex-ensemble-roost.csv").composition
+compositions_1 = pd.read_csv("data/mp-n_elements<2.csv").formula
+compositions_2 = pd.read_csv("data/ex-ensemble-roost.csv").composition
+df_ptable = pd.read_csv("ml_matrics/elements.csv")
 
-counts_a = count_elements(compositions_a)
-counts_b = count_elements(compositions_b)
+elem_counts_1 = count_elements(compositions_1)
+elem_counts_2 = count_elements(compositions_2)
 
 
 def test_hist_elemental_prevalence():
-    hist_elemental_prevalence(compositions_a)
+    hist_elemental_prevalence(compositions_1)
 
 
 def test_hist_elemental_prevalence_log():
-    hist_elemental_prevalence(compositions_a, log=True)
+    hist_elemental_prevalence(compositions_1, log=True)
 
 
 def test_hist_elemental_prevalence_with_keep_top():
-    hist_elemental_prevalence(compositions_a, keep_top=10)
+    hist_elemental_prevalence(compositions_1, keep_top=10)
 
 
-def test_hist_elemental_prevalence_with_bar_values_count():
-    hist_elemental_prevalence(compositions_a, keep_top=10, bar_values="count")
+def test_hist_elemental_prevalence_with_2ar_values_count():
+    hist_elemental_prevalence(compositions_1, keep_top=10, bar_values="count")
 
 
 def test_ptable_heatmap():
-    ptable_heatmap(compositions_a)
+    ptable_heatmap(compositions_1)
+    ptable_heatmap(df_ptable.atomic_mass)
 
 
 def test_ptable_heatmap_log():
-    ptable_heatmap(compositions_a)
+    ptable_heatmap(compositions_1)
 
 
 def test_ptable_heatmap_cbar_max():
-    cbar_max = max(counts_a.max(), counts_b.max())
-    ptable_heatmap(compositions_a, cbar_max=cbar_max)
-    ptable_heatmap(compositions_b, cbar_max=cbar_max)
+    cbar_max = max(elem_counts_1.max(), elem_counts_2.max())
+    ptable_heatmap(compositions_1, cbar_max=cbar_max)
+    ptable_heatmap(compositions_2, cbar_max=cbar_max)
 
 
 def test_ptable_heatmap_with_elem_counts():
-    elem_counts = count_elements(compositions_a)
-    ptable_heatmap(elem_counts=elem_counts)
+    ptable_heatmap(elem_counts_1)
 
 
 def test_ptable_heatmap_ratio():
-    ptable_heatmap_ratio(compositions_a, compositions_b)
+    ptable_heatmap_ratio(compositions_1, compositions_2)
 
 
 def test_ptable_heatmap_ratio_with_elem_counts():
-    elem_counts = count_elements(compositions_a)
-    elem_counts_b = count_elements(compositions_b)
-    ptable_heatmap_ratio(elem_counts_a=elem_counts, elem_counts_b=elem_counts_b)
+    ptable_heatmap_ratio(elem_counts_1, elem_counts_2)
 
 
 def test_ptable_heatmap_ratio_with_mixed():
-    elem_counts_b = count_elements(compositions_b)
-    ptable_heatmap_ratio(formulas_a=compositions_a, elem_counts_b=elem_counts_b)
-    ptable_heatmap_ratio(formulas_b=compositions_a, elem_counts_a=elem_counts_b)
+    ptable_heatmap_ratio(compositions_1, elem_counts_2)
+    ptable_heatmap_ratio(compositions_1, elem_counts_2)
