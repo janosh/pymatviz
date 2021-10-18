c3210bf9363e927b5a1de6a2e1053284e541353c
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-10-18 17:03:14 +0100

    ptable_heatmap add heat_labels kwarg for fraction, percent or None heat labels, make ptable_heatmap_ratio colors and legend customizable

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -18,47 +18,44 @@ elem_counts_2 = count_elements(compositions_2)
 
 def test_hist_elemental_prevalence():
     hist_elemental_prevalence(compositions_1)
-
-
-def test_hist_elemental_prevalence_log():
     hist_elemental_prevalence(compositions_1, log=True)
-
-
-def test_hist_elemental_prevalence_with_keep_top():
     hist_elemental_prevalence(compositions_1, keep_top=10)
-
-
-def test_hist_elemental_prevalence_with_2ar_values_count():
     hist_elemental_prevalence(compositions_1, keep_top=10, bar_values="count")
 
 
 def test_ptable_heatmap():
     ptable_heatmap(compositions_1)
+    ptable_heatmap(compositions_1, log=True)
+
+    # custom color map
+    ptable_heatmap(compositions_1, log=True, cmap="summer")
+
+    # heat_labels normalized to total count
+    ptable_heatmap(compositions_1, heat_labels="fraction")
+    ptable_heatmap(compositions_1, heat_labels="percent")
+
+    # without heatmap values
+    ptable_heatmap(compositions_1, heat_labels=None)
+    ptable_heatmap(compositions_1, log=True, heat_labels=None)
+
+    # element properties as heatmap values
     ptable_heatmap(df_ptable.atomic_mass)
 
+    # custom max color bar value
+    ptable_heatmap(compositions_1, cbar_max=1e2)
+    ptable_heatmap(compositions_1, log=True, cbar_max=1e2)
 
-def test_ptable_heatmap_log():
-    ptable_heatmap(compositions_1)
-
-
-def test_ptable_heatmap_cbar_max():
-    cbar_max = max(elem_counts_1.max(), elem_counts_2.max())
-    ptable_heatmap(compositions_1, cbar_max=cbar_max)
-    ptable_heatmap(compositions_2, cbar_max=cbar_max)
-
-
-def test_ptable_heatmap_with_elem_counts():
+    # element counts
     ptable_heatmap(elem_counts_1)
 
 
 def test_ptable_heatmap_ratio():
+    # composition strings
     ptable_heatmap_ratio(compositions_1, compositions_2)
 
-
-def test_ptable_heatmap_ratio_with_elem_counts():
+    # element counts
     ptable_heatmap_ratio(elem_counts_1, elem_counts_2)
 
-
-def test_ptable_heatmap_ratio_with_mixed():
+    # mixed element counts and composition
     ptable_heatmap_ratio(compositions_1, elem_counts_2)
-    ptable_heatmap_ratio(compositions_1, elem_counts_2)
+    ptable_heatmap_ratio(elem_counts_1, compositions_2)
