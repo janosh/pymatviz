0c0c043bde14f356ddfc2335f5bd900e91c159fa
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-07-07 17:56:42 +0200

    fix flaky CI error from matminer load_dataset()
    replace with hard-coded compositions in test_elements.py
    
    # Check to see if file hash matches the expected value, if hash is provided
    UserWarning: Error, hash of downloaded file does not match that included in metadata, the data may be corrupt or altered

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import pandas as pd
 import pytest
-from matminer.datasets import load_dataset
 from matplotlib.axes import Axes
 from plotly.exceptions import PlotlyError
 from plotly.graph_objs._figure import Figure
@@ -19,24 +18,40 @@ from pymatviz.utils import df_ptable
 
 
 @pytest.fixture
-def glasses() -> pd.Series[Composition]:
-    return load_dataset("matbench_glass").composition
+def glass_formulas() -> list[str]:
+    """The result of
+    from matminer.datasets import load_dataset
+
+    load_dataset("matbench_glass").composition.head(20)
+    """
+    return (
+        "Al,Al(NiB)2,Al10Co21B19,Al10Co23B17,Al10Co27B13,Al10Co29B11,Al10Co31B9,"
+        "Al10Co33B7,Al10Cr3Si7,Al10Fe23B17,Al10Fe27B13,Al10Fe31B9,Al10Fe33B7,"
+        "Al10Ni23B17,Al10Ni27B13,Al10Ni29B11,Al10Ni31B9,Al10Ni33B7,Al11(CrSi2)3"
+    ).split(",")
 
 
 @pytest.fixture
-def glass_elem_counts(glasses: pd.Series[Composition]) -> pd.Series[int]:
-    return count_elements(glasses)
+def glass_elem_counts(glass_formulas: pd.Series[Composition]) -> pd.Series[int]:
+    return count_elements(glass_formulas)
 
 
 @pytest.fixture
-def steels() -> pd.Series[Composition]:
-    # unusually fractional compositions, good for testing edge cases
-    return load_dataset("matbench_steels").composition
+def steel_formulas() -> list[str]:
+    """Unusually fractional compositions, good for testing edge cases. The result of:
+    from matminer.datasets import load_dataset
+
+    load_dataset("matbench_steels").composition.head(2)
+    """
+    return [
+        "Fe0.620C0.000953Mn0.000521Si0.00102Cr0.000110Ni0.192Mo0.0176V0.000112Nb0.0000616Co0.146Al0.00318Ti0.0185",  # noqa: E501
+        "Fe0.623C0.00854Mn0.000104Si0.000203Cr0.147Ni0.0000971Mo0.0179V0.00515N0.00163Nb0.0000614Co0.188W0.00729Al0.000845",  # noqa: E501
+    ]
 
 
 @pytest.fixture
-def steel_elem_counts(steels: pd.Series[Composition]) -> pd.Series[int]:
-    return count_elements(steels)
+def steel_elem_counts(steel_formulas: pd.Series[Composition]) -> pd.Series[int]:
+    return count_elements(steel_formulas)
 
 
 @pytest.mark.parametrize(
@@ -67,33 +82,33 @@ def test_count_elements_bad_atomic_nums(rng):
         count_elements({str(idx): 0 for idx in list(range(*rng))})
 
 
-def test_hist_elemental_prevalence(glasses):
-    ax = hist_elemental_prevalence(glasses)
+def test_hist_elemental_prevalence(glass_formulas):
+    ax = hist_elemental_prevalence(glass_formulas)
     assert isinstance(ax, Axes)
 
-    hist_elemental_prevalence(glasses, log=True)
+    hist_elemental_prevalence(glass_formulas, log=True)
 
-    hist_elemental_prevalence(glasses, keep_top=10)
+    hist_elemental_prevalence(glass_formulas, keep_top=10)
 
-    hist_elemental_prevalence(glasses, keep_top=10, bar_values="count")
+    hist_elemental_prevalence(glass_formulas, keep_top=10, bar_values="count")
 
 
-def test_ptable_heatmap(glasses, glass_elem_counts):
-    ax = ptable_heatmap(glasses)
+def test_ptable_heatmap(glass_formulas, glass_elem_counts):
+    ax = ptable_heatmap(glass_formulas)
     assert isinstance(ax, Axes)
 
-    ptable_heatmap(glasses, log=True)
+    ptable_heatmap(glass_formulas, log=True)
 
     # custom color map
-    ptable_heatmap(glasses, log=True, cmap="summer")
+    ptable_heatmap(glass_formulas, log=True, cmap="summer")
 
     # heat_labels normalized to total count
-    ptable_heatmap(glasses, heat_labels="fraction")
-    ptable_heatmap(glasses, heat_labels="percent")
+    ptable_heatmap(glass_formulas, heat_labels="fraction")
+    ptable_heatmap(glass_formulas, heat_labels="percent")
 
     # without heatmap values
-    ptable_heatmap(glasses, heat_labels=None)
-    ptable_heatmap(glasses, log=True, heat_labels=None)
+    ptable_heatmap(glass_formulas, heat_labels=None)
+    ptable_heatmap(glass_formulas, log=True, heat_labels=None)
 
     # element properties as heatmap values
     ptable_heatmap(df_ptable.atomic_mass)
@@ -102,14 +117,14 @@ def test_ptable_heatmap(glasses, glass_elem_counts):
     ptable_heatmap(df_ptable.atomic_mass, text_color=("red", "blue"))
 
     # custom max color bar value
-    ptable_heatmap(glasses, cbar_max=1e2)
-    ptable_heatmap(glasses, log=True, cbar_max=1e2)
+    ptable_heatmap(glass_formulas, cbar_max=1e2)
+    ptable_heatmap(glass_formulas, log=True, cbar_max=1e2)
 
     # element counts
     ptable_heatmap(glass_elem_counts)
 
     with pytest.raises(ValueError, match="Combining log color scale and"):
-        ptable_heatmap(glasses, log=True, heat_labels="percent")
+        ptable_heatmap(glass_formulas, log=True, heat_labels="percent")
 
     ptable_heatmap(glass_elem_counts, exclude_elements=["O", "P"])
 
@@ -117,48 +132,52 @@ def test_ptable_heatmap(glasses, glass_elem_counts):
         ptable_heatmap(glass_elem_counts, exclude_elements=["foobar"])
 
 
-def test_ptable_heatmap_ratio(steels, glasses, steel_elem_counts, glass_elem_counts):
+def test_ptable_heatmap_ratio(
+    steel_formulas, glass_formulas, steel_elem_counts, glass_elem_counts
+):
     # composition strings
-    ax = ptable_heatmap_ratio(glasses, steels)
+    ax = ptable_heatmap_ratio(glass_formulas, steel_formulas)
     assert isinstance(ax, Axes)
 
     # element counts
     ptable_heatmap_ratio(glass_elem_counts, steel_elem_counts, normalize=True)
 
     # mixed element counts and composition
-    ptable_heatmap_ratio(glasses, steel_elem_counts)
-    ptable_heatmap_ratio(glass_elem_counts, steels)
+    ptable_heatmap_ratio(glass_formulas, steel_elem_counts)
+    ptable_heatmap_ratio(glass_elem_counts, steel_formulas)
 
 
-def test_ptable_heatmap_plotly(glasses):
-    fig = ptable_heatmap_plotly(glasses)
+def test_ptable_heatmap_plotly(glass_formulas):
+    fig = ptable_heatmap_plotly(glass_formulas)
     assert isinstance(fig, Figure)
     assert len(fig.layout.annotations) == 18 * 10  # n_cols * n_rows
     assert sum(anno.text != "" for anno in fig.layout.annotations) == 118  # n_elements
 
     ptable_heatmap_plotly(
-        glasses, hover_props=["atomic_mass", "atomic_number", "density"]
+        glass_formulas, hover_props=["atomic_mass", "atomic_number", "density"]
     )
     ptable_heatmap_plotly(
-        glasses,
+        glass_formulas,
         hover_data="density = " + df_ptable.density.astype(str) + " g/cm^3",
     )
     ptable_heatmap_plotly(df_ptable.density, precision=".1f")
 
-    ptable_heatmap_plotly(glasses, heat_labels="percent")
+    ptable_heatmap_plotly(glass_formulas, heat_labels="percent")
 
-    # test that bad colorscale raises ValueError
     with pytest.raises(ValueError, match="should be string, list of strings or list"):
-        ptable_heatmap_plotly(glasses, colorscale=lambda: "foobar")  # type: ignore
+        # test that bad colorscale raises ValueError
+        ptable_heatmap_plotly(
+            glass_formulas, colorscale=lambda: "bad color scale"  # type: ignore
+        )
 
     # test that unknown builtin colorscale raises ValueError
     with pytest.raises(PlotlyError, match="Colorscale foobar is not a built-in scale"):
-        ptable_heatmap_plotly(glasses, colorscale="foobar")
+        ptable_heatmap_plotly(glass_formulas, colorscale="foobar")
 
 
 @pytest.mark.parametrize(
     "clr_scl", ["YlGn", ["blue", "red"], [(0, "blue"), (1, "red")]]
 )
-def test_ptable_heatmap_plotly_colorscale(glasses, clr_scl):
-    fig = ptable_heatmap_plotly(glasses, colorscale=clr_scl)
+def test_ptable_heatmap_plotly_colorscale(glass_formulas, clr_scl):
+    fig = ptable_heatmap_plotly(glass_formulas, colorscale=clr_scl)
     assert isinstance(fig, Figure)
