ea3ff32b84274402bd20af4b0c4e0ca3f6545058
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-30 17:36:24 -0800

    deploy docs to GitHub Pages https://janosh.github.io/pymatviz
    only readme as landing page and API docs for now
    hopefully interactive plotly demos to come

---
+++ site/src/routes/api/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let data
+</script>
+
+{@html data.html}
+
+<style>
+  :global(hr) {
+    border: none;
+    margin: 3em;
+  }
+  :global(code) {
+    line-height: 1em;
+    border-radius: 4pt;
+  }
+</style>
