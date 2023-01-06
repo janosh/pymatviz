6319379ee02f4185599aa5d95308d3274091662f
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-06 07:43:23 -0800

    tweak docs CSS, add site footer

--- site/src/routes/api/+page.svelte
+++ site/src/routes/api/+page.svelte
@@ -2,12 +2,17 @@
   export let data
 </script>
 
+<h1 class="toc-exclude">API</h1>
+
 {@html data.html}
 
 <style>
+  /* select all but first module h1s */
+  :global(h1[id^='module-']:not(:nth-of-type(2))) {
+    margin: 4em 0 0;
+  }
   :global(hr) {
     border: none;
-    margin: 3em;
   }
   :global(code) {
     line-height: 1em;
