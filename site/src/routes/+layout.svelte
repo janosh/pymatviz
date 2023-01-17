7da3c0c51ea43871a1a8ee6d9886506d16a5f601
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-17 13:01:32 -0800

    use redirect in layout.ts instead of ugly DOM href surgery to forward readme links to GH  repo
    update Footer

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -2,7 +2,6 @@
   import { page } from '$app/stores'
   import Footer from '$lib/Footer.svelte'
   import { repository } from '$site/package.json'
-  import { onMount } from 'svelte'
   import Toc from 'svelte-toc'
   import { GitHubCorner } from 'svelte-zoo'
   import '../app.css'
@@ -10,16 +9,6 @@
   $: headingSelector = `main :is(${
     $page.url.pathname === `/api` ? `h1, h2, h3, h4` : `h2`
   }):not(.toc-exclude)`
-
-  const site_url = `https://janosh.github.io/pymatviz`
-  onMount(() => {
-    for (const link of [
-      ...document.querySelectorAll(`a[href^='${site_url}']`),
-    ] as HTMLAnchorElement[]) {
-      link.href = link.href.replace(site_url, ``)
-      link.text = link.text.replace(site_url, ``)
-    }
-  })
 </script>
 
 <Toc {headingSelector} breakpoint={1250} warnOnEmpty={false} />
