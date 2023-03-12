17df4d46b67f10f0c45c522729c2a3f349154220
Hui Zheng <huz071@eng.ucsd.edu>
2023-03-11 18:47:30 -0800

    Add `ptable_heatmap` `cbar_precision` kwarg (#70)
    * add cbar_precision arg
    
    * update pre-commit hooks
    
    * add new ptable_heatmap cbar_precision kwarg to doc str
    
    * add svelte-multiselect CmdPalette for keyboard-only site navigation
    
    * fix mypy
    
    ---------
    
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
   import { page } from '$app/stores'
   import Footer from '$lib/Footer.svelte'
   import { repository } from '$site/package.json'
+  import { CmdPalette } from 'svelte-multiselect'
   import Toc from 'svelte-toc'
   import { GitHubCorner } from 'svelte-zoo'
   import '../app.css'
@@ -9,7 +11,18 @@
   $: headingSelector = `main :is(${
     $page.url.pathname === `/api` ? `h1, h2, h3, h4` : `h2`
   }):not(.toc-exclude)`
+
+  const actions = Object.keys(import.meta.glob(`./**/+page.{svx,svelte,md}`)).map(
+    (filename) => {
+      const parts = filename.split(`/`).filter((part) => !part.startsWith(`(`)) // remove hidden route segments
+      const route = `/${parts.slice(1, -1).join(`/`)}`
+
+      return { label: route, action: () => goto(route) }
+    }
+  )
 </script>
+
+<CmdPalette {actions} placeholder="Go to..." />
 
 <Toc {headingSelector} breakpoint={1250} warnOnEmpty={false} />
 
