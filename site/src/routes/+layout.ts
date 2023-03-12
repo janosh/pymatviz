55051a6f9e655f42974c1845e95b7cdd261b1bef
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-11 16:40:38 -0800

    add new option 'occurrence' for CountMode = element_composition|fractional_composition|reduced_composition
    add kwarg fill_value to count_elements()
    fix test_spacegroup_hist_num()
    
    ruff ignore PLW2901

--- site/src/routes/+layout.ts
+++ site/src/routes/+layout.ts
@@ -1,9 +1,8 @@
 import { redirect } from '@sveltejs/kit'
-import type { LayoutLoad } from './$types'
 
 export const prerender = true
 
-export const load: LayoutLoad = ({ url }) => {
+export const load = ({ url }) => {
   if (url.pathname.match(`^/(examples|pymatviz)`)) {
     const gh_file_url = `https://github.com/janosh/pymatviz/blob/main/${url.pathname}`
     throw redirect(307, gh_file_url)
