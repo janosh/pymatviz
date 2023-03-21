fa215ef54bc909e30099c9d0ac93d583f7bdcfb6
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-03-21 14:26:54 -0700

    add changelog.md via auto-changelog

---
+++ site/src/routes/changelog/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Changelog from '$root/changelog.md'
+  import Toc from 'svelte-toc'
+</script>
+
+<main>
+  <Changelog />
+</main>
+
+<Toc headingSelector="main > :where(h4)" breakpoint={1250} />
