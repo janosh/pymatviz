46cf5481bb60de0920b4a2bd6796ad57a762d526
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-04-29 15:33:30 -0700

    parse title from leading comment in example snippets

--- site/src/routes/[slug]/+page.server.ts
+++ site/src/routes/[slug]/+page.server.ts
@@ -13,7 +13,16 @@ export const load = async ({ params }) => {
   if (matches.length == 0) return { status: 404 }
 
   const examples = matches.map(async (path) => {
-    const code = raw[path]
+    let code = raw[path]
+    const name = path.split(`/`).at(-1)?.split(`.`)[1] ?? ``
+    let title = name.replace(/-/g, ` `)
+
+    if (code.startsWith(`# `)) {
+      // split 1st line into title and code
+      const lines = code.split(`\n\n`)
+      title = lines[0].replace(`# `, ``)
+      code = lines.slice(1).join(`\n\n`)
+    }
     const compiled = (await compile(`\`\`\`py\n${code}\n\`\`\``))?.code
     if (!compiled) throw new Error(`Failed to highlight ${path}`)
 
@@ -22,8 +31,6 @@ export const load = async ({ params }) => {
       ?.replace(/>{@html `<code class="language-/g, `><code class="language-`)
       .replace(/<\/code>`}<\/pre>/g, `</code></pre>`)
 
-    const name = path.split(`/`).at(-1)?.split(`.`)[1] ?? ``
-    const title = name.replace(/-/g, ` `)
     return { title, name, path, code, highlighted }
   })
 
