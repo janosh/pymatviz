b240fcc316c32e7dca2939d581e1c3cce804701e
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-09 14:47:19 -0700

    Split `pymatviz/io.py` out from `pymatviz/utils.py` and add `df_to_pdf()` export function (#91)
    * pre-commit autoupdate
    
    * add optional deps df-pdf-export = ["pdfCropMargins", "weasyprint"]
    
    * pymatviz/utils.py add df_to_pdf() and normalize_and_crop_pdf()
    
    * add test_df_to_pdf()
    
    * split pymatviz/io.py out from pymatviz/utils.py
    
    * split test_io.py out from test_utils.py
    
    * remove 'body { margin: 0; padding: 1em; }' since curly braces break docs
    
    * add jinja2 to df-pdf-export extra deps
    
    * fix outdated import from pymatviz.(utils->io) import save_and_compress_svg
    
    * fix dead link
    
    * add jinja2 to test deps

--- site/src/routes/[slug]/parity/6.scatter-with-err-bar.py
+++ site/src/routes/[slug]/parity/6.scatter-with-err-bar.py
@@ -1,7 +1,7 @@
 import numpy as np
 
+from pymatviz.io import save_and_compress_svg
 from pymatviz.parity import residual_vs_actual
-from pymatviz.utils import save_and_compress_svg
 
 
 rand_regression_size = 500
--- site/src/routes/[slug]/ptable/2.ptable-heatmap-atomic-mass.py
+++ site/src/routes/[slug]/ptable/2.ptable-heatmap-atomic-mass.py
@@ -1,7 +1,8 @@
 # Periodic Table Heatmap
 
+from pymatviz.io import save_and_compress_svg
 from pymatviz.ptable import ptable_heatmap
-from pymatviz.utils import df_ptable, save_and_compress_svg
+from pymatviz.utils import df_ptable
 
 
 ax = ptable_heatmap(df_ptable.atomic_mass)
