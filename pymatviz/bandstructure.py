e34f71c47ae309a1c2eddc874564f55cfb2114e2
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-11-18 14:46:23 -0800

    make sure each bandstructure only has single legend handle

--- pymatviz/bandstructure.py
+++ pymatviz/bandstructure.py
@@ -28,79 +28,77 @@ def plot_band_structure(
     """
     fig = go.Figure()
 
-    if isinstance(band_structs, PhononBandStructureSymmLine):
-        band_structs = {"Single BS": band_structs}
+    # Normalize input to a dictionary
+    if not isinstance(band_structs, dict):
+        band_structs = {"": band_structs}
+    colors = iter(px.colors.qualitative.Plotly)
 
-    # Define a list of colors for each band structure
-    # symbol_iter = iter(SymbolValidator().values[2::6])
-    colors = iter(px.colors.qualitative.Dark24)
-
-    # Find the common branches
+    # Find common branches by normalized branch names
     common_branches = None
     for bs in band_structs.values():
-        branches = {b["name"] for b in bs.branches}
-        if common_branches is None:
-            common_branches = branches
-        else:
-            common_branches &= branches
-    if common_branches is None:
+        branches = {b["name"].replace("GAMMA", "Γ") for b in bs.branches}
+        common_branches = (
+            branches if common_branches is None else common_branches & branches
+        )
+
+    if not common_branches:
         raise ValueError("No common branches found among the band structures.")
 
     # Plotting only the common branches for each band structure
     for label, bs in band_structs.items():
-        line_color = next(colors, "blue")  # Use blue if colorway is exhausted
-        for band in range(bs.nb_bands):
-            for branch in bs.branches:
-                if branch["name"] in common_branches:
-                    start_index = branch["start_index"]
-                    end_index = branch["end_index"] + 1  # Include the end point
-                    distances = bs.distance[start_index:end_index]
-                    frequencies = bs.bands[band, start_index:end_index]
+        color = next(colors)
+        for b in bs.branches:
+            normalized_name = b["name"].replace("GAMMA", "Γ")
+            if normalized_name in common_branches:
+                start_index = b["start_index"]
+                end_index = b["end_index"] + 1  # Include the end point
+                distances = bs.distance[start_index:end_index]
+                for band in range(bs.nb_bands):
+                    frequencies = bs.bands[band][start_index:end_index]
+                    # Group traces for toggling and set legend name only for the first band
+                    legend_name = label if band == 0 else None
                     fig.add_trace(
                         go.Scatter(
                             x=distances,
                             y=frequencies,
                             mode="lines",
-                            line=dict(color=line_color),
-                            name=label,
+                            line=dict(color=color),
+                            legendgroup=label,
+                            name=legend_name,
+                            showlegend=band == 0,  # Show legend only for the first band
                         )
                     )
 
-    # Adding vertical lines at high-symmetry points for the first band structure
+    # Add vertical lines for common high-symmetry points
     first_bs = next(iter(band_structs.values()))
     high_symm_distances = [
         first_bs.distance[first_bs.branches[i]["start_index"]]
         for i in range(len(first_bs.branches))
-        if first_bs.branches[i]["name"] in common_branches
+        if first_bs.branches[i]["name"].replace("GAMMA", "Γ") in common_branches
     ]
 
     for dist in high_symm_distances:
         fig.add_vline(x=dist, line=dict(color="black", width=1))
 
-    # Customizing the plot appearance
+    # Set x-axis and y-axis range
+    all_frequencies = np.concatenate(
+        [bs.bands.flatten() for bs in band_structs.values()]
+    )
     fig.update_layout(
         title="Phonon Band Structure",
         xaxis_title="Wave Vector",
         yaxis_title="Frequency (THz)",
         xaxis=dict(
-            range=[min(high_symm_distances), max(high_symm_distances)],
             tickmode="array",
             tickvals=high_symm_distances,
-            ticktext=[
-                first_bs.qpoints[first_bs.branches[i]["start_index"]].label.replace(
-                    "GAMMA", "Γ"
-                )
-                for i in range(len(first_bs.branches))
-                if first_bs.branches[i]["name"] in common_branches
-            ],
+            ticktext=[b.replace("GAMMA", "Γ") for b in common_branches],
         ),
-        yaxis=dict(range=[np.amin(first_bs.bands), np.amax(first_bs.bands)]),
+        yaxis=dict(range=[np.amin(all_frequencies), np.amax(all_frequencies)]),
         plot_bgcolor="white",
         margin=dict(t=40),  # Adjust top margin to ensure title fits
     )
 
-    # Enable legend toggle only for multiple band structures
-    if len(band_structs) > 1:
-        fig.update_layout(legend=dict(itemclick="toggleothers"))
+    # Enable toggling traces by clicking on the legend
+    fig.update_layout(legend=dict(itemclick="toggleothers"))
 
     return fig
