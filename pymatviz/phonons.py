232e7fd0518c4c05c3bde7b2d80b285700b7e5e9
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-02-02 19:22:38 +0800

    Minor format tweaks (#120)
    * minor format tweaks
    
    * bump ruff to 0.2.0
    
    * revert readme
    
    * get mpl_key, plotly_key from type Backend, used in pretty_metric_label
    
    * delete mixing-enthalpy.csv.gz
    
    ---------
    
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

--- pymatviz/phonons.py
+++ pymatviz/phonons.py
@@ -138,10 +138,7 @@ def plot_phonon_bands(
                 f"Only {PhononBands.__name__} objects supported, got {type_name}"
             )
         branches = {pretty_sym_point(branch["name"]) for branch in bs.branches}
-        if not common_branches:
-            common_branches = branches
-        else:
-            common_branches = common_branches & branches
+        common_branches = common_branches & branches if common_branches else branches
 
     if not common_branches:
         raise ValueError("No common branches found among the band structures.")
