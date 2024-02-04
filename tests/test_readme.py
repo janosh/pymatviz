d4b25111aa4f8a39065dc8a782faa07dcf2c8afb
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-02-04 19:37:02 +0800

    use explicit encoding and absolute path

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,12 +1,17 @@
-import os
+from pathlib import Path
 
 
 def test_no_missing_images() -> None:
     """Test that all images in the readme are present in repo."""
-    with open("readme.md") as file:
+    project_dir = Path(__file__).resolve().parent.parent
+    assets_dir = project_dir / "assets"
+
+    with open(project_dir / "readme.md", encoding="utf-8") as file:
         readme = file.read()
+
     base_url = "https://raw.githubusercontent.com/janosh/pymatviz/main/assets/"
     images = [text.split(".svg\n")[0] for text in readme.split(base_url)[1:]]
 
     for idx, img in enumerate(images, 1):
-        assert os.path.isfile(f"assets/{img}.svg"), f"Missing readme {img=} ({idx=})"
+        img_path = assets_dir / f"{img}.svg"
+        assert img_path.is_file(), f"Missing readme {img=} ({idx=})"
