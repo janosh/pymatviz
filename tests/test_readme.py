d958382b70c152a275723ea781b0e85cd5cfd0a8
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-02-08 20:01:03 +0100

    use shorter version of GH raw URLs

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,17 +1,14 @@
-from pathlib import Path
+import os
+
+from pymatviz import ROOT
 
 
 def test_no_missing_images() -> None:
     """Test that all images in the readme are present in repo."""
-    project_dir = Path(__file__).resolve().parent.parent
-    assets_dir = project_dir / "assets"
-
-    with open(project_dir / "readme.md", encoding="utf-8") as file:
+    with open(f"{ROOT}/readme.md") as file:
         readme = file.read()
-
-    base_url = "https://raw.githubusercontent.com/janosh/pymatviz/main/assets/"
+    base_url = "https://github.com/janosh/pymatviz/raw/main/assets/"
     images = [text.split(".svg\n")[0] for text in readme.split(base_url)[1:]]
 
     for idx, img in enumerate(images, 1):
-        img_path = assets_dir / f"{img}.svg"
-        assert img_path.is_file(), f"Missing readme {img=} ({idx=})"
+        assert os.path.isfile(f"assets/{img}.svg"), f"Missing readme {img=} ({idx=})"
