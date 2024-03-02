4d3091edb209621c2dcc0d299f34a1b039fea8de
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-03-02 08:33:01 +0100

    breaking: rename fmt_spec keyword to fmt in si_fmt() for code base consistency
    fix docs build due to broken internal links and HTML <tags> in doc io.py doc strings
    update pre-commit hooks

--- site/src/routes/+layout.ts
+++ site/src/routes/+layout.ts
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit'
 export const prerender = true
 
 export const load = ({ url }) => {
-  if (url.pathname.match(`^/(examples|pymatviz)`)) {
+  if (url.pathname.match(`^/(examples|pymatviz|citation)`)) {
     const gh_file_url = `https://github.com/janosh/pymatviz/blob/-${url.pathname}`
     throw redirect(307, gh_file_url)
   }
