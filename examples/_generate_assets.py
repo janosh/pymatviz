96a5773dbaa3c76957146a42c121d3bd67778a0d
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-10 20:45:44 +0800

    add sketch (color not working)

--- examples/_generate_assets.py
+++ examples/_generate_assets.py
@@ -1,5 +1,6 @@
 # %%
 import json
+import random
 from glob import glob
 
 import matplotlib.pyplot as plt
@@ -34,6 +35,7 @@ from pymatviz.phonons import (
     plot_phonon_dos,
 )
 from pymatviz.ptable import (
+    ptable_diags,
     ptable_heatmap,
     ptable_heatmap_plotly,
     ptable_heatmap_ratio,
@@ -48,7 +50,7 @@ from pymatviz.uncertainty import error_decay_with_uncert, qq_gaussian
 from pymatviz.utils import TEST_FILES, df_ptable
 
 
-# %% configure matplotlib and load test data
+# %% Configure matplotlib and load test data
 plt.rc("font", size=14)
 plt.rc("savefig", bbox="tight", dpi=200)
 plt.rc("axes", titlesize=16, titleweight="bold")
@@ -58,7 +60,7 @@ plt.rcParams["figure.constrained_layout.use"] = True
 px.defaults.template = "pymatviz_white"
 pio.templates.default = "pymatviz_white"
 
-# random classification data
+# Random classification data
 np.random.seed(42)
 rand_clf_size = 100
 y_binary = np.random.choice([0, 1], size=rand_clf_size)
@@ -67,7 +69,7 @@ y_proba = np.clip(
 )
 
 
-# random regression data
+# Random regression data
 rand_regression_size = 500
 y_true = np.random.normal(5, 4, rand_regression_size)
 y_pred = 1.2 * y_true - 2 * np.random.normal(0, 1, rand_regression_size)
@@ -164,7 +166,7 @@ save_and_compress_svg(fig, "ptable-heatmap-plotly-log")
 
 
 # %% Histograms laid out in as a periodic table
-# generate random parity data with y \approx x with some noise
+# Generate random parity data with y \approx x with some noise
 data_dict = {
     elem.symbol: np.random.randn(100) + np.random.randn(100) for elem in Element
 }
@@ -191,6 +193,23 @@ fig = ptable_plots(
     plot_kwds=dict(marker="o", linestyle=""),
 )
 save_and_compress_svg(fig, "ptable-scatters")
+
+
+# %% Diagonally-split tile plots laid out as a periodic table
+data_dict = {
+    elem.symbol: [
+        random.randint(0, 10),
+        random.randint(10, 20),
+    ]
+    for elem in Element
+}
+
+fig = ptable_diags(
+    data_dict,
+    colormap="coolwarm",
+    cbar_title="Periodic Table Diagonally-Split Tiles Plots",
+)
+save_and_compress_svg(fig, "ptable-diags")
 
 
 # %% Uncertainty Plots
@@ -261,7 +280,7 @@ save_and_compress_svg(fig, "spg-symbol-sunburst")
 
 
 # %% Correlation Plots
-# plot eigenvalue distribution of a pure-noise correlation matrix
+# Plot eigenvalue distribution of a pure-noise correlation matrix
 # i.e. the correlation matrix contains no significant correlations
 # beyond the spurious correlation that occurs randomly
 n_rows, n_cols = 500, 1000
@@ -272,7 +291,7 @@ corr_mat = np.corrcoef(rand_wide_mat)
 ax = marchenko_pastur(corr_mat, gamma=n_cols / n_rows)
 save_and_compress_svg(ax, "marchenko-pastur")
 
-# plot eigenvalue distribution of a correlation matrix with significant
+# Plot eigenvalue distribution of a correlation matrix with significant
 # (i.e. non-noise) eigenvalue
 n_rows, n_cols = 50, 400
 linear_matrix = np.arange(n_rows * n_cols).reshape(n_rows, n_cols) / n_cols
@@ -282,7 +301,7 @@ corr_mat = np.corrcoef(linear_matrix + rand_wide_mat[:n_rows, :n_cols])
 ax = marchenko_pastur(corr_mat, gamma=n_cols / n_rows)
 save_and_compress_svg(ax, "marchenko-pastur-significant-eval")
 
-# plot eigenvalue distribution of a rank-deficient correlation matrix
+# Plot eigenvalue distribution of a rank-deficient correlation matrix
 n_rows, n_cols = 600, 500
 rand_tall_mat = np.random.normal(0, 1, size=(n_rows, n_cols))
 
