a0926f534fefb3859735a0affb96cd85e06f0625
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-16 10:06:41 +0800

    use 2-split as example

--- examples/_generate_assets.py
+++ examples/_generate_assets.py
@@ -198,8 +198,9 @@ save_and_compress_svg(fig, "ptable-scatters")
 # %% Evenly-split tile plots laid out as a periodic table
 data_dict = {
     elem.symbol: [
-        random.randint(0, 10)
-        for _ in range(random.randint(1, 4))  # random length
+        random.randint(0, 10),
+        random.randint(10, 20),
+        # random.randint(20, 30),
     ]
     for elem in Element
 }
