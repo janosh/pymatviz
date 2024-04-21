90bb3482166e3860d98524890a9466db2c8ba117
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-04-21 11:34:46 +0800

    add placeholders

--- pymatviz/ptable.py
+++ pymatviz/ptable.py
@@ -961,204 +961,80 @@ def ptable_hists(
     return fig
 
 
-def ptable_plots(
+def ptable_scatters(
     data: pd.DataFrame | pd.Series | dict[str, list[list[float]]],
     plot_kwds: dict[str, Any]
     | Callable[[Sequence[float]], dict[str, Any]]
     | None = None,
     colormap: str | None = None,
-    ax_kwds: dict[str, Any] | None = None,
-    symbol_kwargs: dict[str, Any] | None = None,
-    symbol_text: str | Callable[[Element], str] = lambda elem: elem.symbol,
-    symbol_pos: tuple[float, float] = (0.5, 0.8),
+    # ax_kwds: dict[str, Any] | None = None,
+    on_empty: Literal["hide", "show"] = "hide",
+) -> plt.Figure:
+    plotter = PTableProjector(
+        colormap=colormap,
+        data=data,
+        # **plot_kwds,  # TODO
+    )
+
+    # Call child plotter: scatter
+    # child_args = {
+    # }
+
+    plotter.add_child_plots(
+        ChildPlotters.scatter,
+        # child_args=child_args,
+        on_empty=on_empty,
+    )
+
+    # Add colorbar
+    plotter.add_colorbar(
+        title=cbar_title,
+        coords=cbar_coords,
+        cbar_kwds=cbar_kwds,
+        title_kwds=cbar_title_kwds,
+    )
+
+    return plotter.fig
+
+
+def ptable_lines(
+    data: pd.DataFrame | pd.Series | dict[str, list[list[float]]],
+    plot_kwds: dict[str, Any]
+    | Callable[[Sequence[float]], dict[str, Any]]
+    | None = None,
+    # ax_kwds: dict[str, Any] | None = None,
     cbar_coords: tuple[float, float, float, float] = (0.18, 0.8, 0.42, 0.02),
     cbar_title: str = "Values",
     cbar_title_kwds: dict[str, Any] | None = None,
     cbar_kwds: dict[str, Any] | None = None,
-    anno_kwds: dict[str, Any] | None = None,
     on_empty: Literal["hide", "show"] = "hide",
-    **kwargs: Any,
 ) -> plt.Figure:
-    """Make scatter or line plots for each element, nested inside a periodic table.
+    plotter = PTableProjector(
+        colormap=colormap,
+        data=data,
+        # **plot_kwds,  # TODO
+    )
 
-    Args:
-        data (pd.DataFrame | pd.Series | dict[str, list[list[float]]]):
-            Map from element symbols to plot data. E.g. if dict,
-            {"Fe": [[1, 2, 3], [4, 5, 6]], "O": [[7, 8], [9, 10]]}.
-            You could also add a 3rd list for coloring, e.g.
-            {"Fe": [[1, 2], [3, 4], [5, 6]]}.
-            If pd.Series, index is element symbols and values lists.
-            If pd.DataFrame, column names are element symbols,
-            plots are created from each column.
-        plot_kwds (dict | Callable): Keywords passed to ax.plot() for each subplot.
-            If callable, it is called with the plot data for each element and
-            should return a dict of keyword arguments. Useful for setting markers,
-            colors, etc. Example: use plot_kwds=dict(marker="o", linestyle="") for
-            scatter (default is line) or as dynamic callable:
-            plot_kwds=lambda plot_vals: dict(
-                linestyle="-" if len(plot_vals) > 40 else ""
-            )
-        colormap (str): Matplotlib colormap name to use. Defaults to None.
-        ax_kwds (dict): Keyword arguments passed to ax.set() for each plot.
-            Use to set x/y labels, limits, etc. Defaults to None. Example:
-            dict(title="Periodic Table", xlabel="x-axis", ylabel="y-axis", xlim=(0, 10),
-            ylim=(0, 10), xscale="linear", yscale="log"). See ax.set() docs for options:
-            https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.set.html#matplotlib-axes-axes-set
-        symbol_text (str | Callable[[Element], str]): Text to display for
-            each element symbol. Defaults to lambda elem: elem.symbol.
-        symbol_kwargs (dict): Keyword arguments passed to plt.text() for
-            element symbols. Defaults to None.
-        symbol_pos (tuple[float, float]): Position of element symbols
-            relative to the lower left corner of each tile.
-            Defaults to (0.5, 0.8). (1, 1) is the upper right corner.
-        cbar_coords (tuple[float, float, float, float]): Colorbar
-            position and size: [x, y, width, height] anchored at lower left
-            corner of the bar. Defaults to (0.25, 0.77, 0.35, 0.02).
-        cbar_title (str): Colorbar title. Defaults to "Values".
-        cbar_title_kwds (dict): Keyword arguments passed to
-            cbar.ax.set_title(). Defaults to dict(fontsize=12, pad=10).
-        cbar_kwds (dict): Keyword arguments passed to fig.colorbar().
-        anno_kwds (dict): Keyword arguments passed to plt.annotate()
-            for element annotations. Defaults to None. Useful for adding
-            e.g. number of data points in each plot. For that, use
-            anno_kwds=lambda plot_vals: dict(text=len(plot_vals)).
-            Recognized keys are text, xy, xycoords, fontsize, and any other
-            plt.annotate() keywords.
-        on_empty ('hide' | 'show'): Whether to show or hide tiles for elements without
-            data. Defaults to "hide".
-        **kwargs: Additional keyword arguments passed to plt.subplots().
+    # Call child plotter: line
+    # child_args = {
 
-    Notes:
-        Default figsize is set to (0.75 * n_groups, 0.75 * n_periods).
+    # }
 
-    Returns:
-        plt.Figure: periodic table with a subplot in each element tile.
-    """
-    n_periods = df_ptable.row.max()
-    n_groups = df_ptable.column.max()
+    plotter.add_child_plots(
+        ChildPlotters.line,
+        # child_args=child_args,
+        on_empty=on_empty,
+    )
 
-    kwargs.setdefault("figsize", (0.75 * n_groups, 0.75 * n_periods))
-    fig, axes = plt.subplots(n_periods, n_groups, **kwargs)
+    # Add colorbar
+    plotter.add_colorbar(
+        title=cbar_title,
+        coords=cbar_coords,
+        cbar_kwds=cbar_kwds,
+        title_kwds=cbar_title_kwds,
+    )
 
-    # Use series name as colorbar title if available when no title was passed
-    if isinstance(data, pd.Series) and cbar_title == "Values" and data.name:
-        cbar_title = data.name
-        data = data.to_dict()
-    elif isinstance(data, pd.DataFrame):
-        data = data.to_dict(orient="list")
-    # data is guaranteed to be a dict from here (should have element symbols as keys)
-
-    cmap = plt.get_cmap(colormap) if colormap else None
-
-    # Turn off axis of subplots on the grid that don't correspond to elements
-    ax: plt.Axes
-    for ax in axes.flat:
-        ax.axis("off")
-
-    symbol_kwargs = symbol_kwargs or {}
-    symbol_kwargs.setdefault("fontsize", 10)
-
-    for element in Element:
-        symbol = element.symbol
-        row, group = df_ptable.loc[symbol, ["row", "column"]]
-
-        ax = axes[row - 1][group - 1]
-        plot_data = np.array(data.get(symbol, []))
-        if len(plot_data) == 0 and on_empty == "hide":
-            continue
-
-        ax.text(
-            *symbol_pos,
-            symbol_text(element)
-            if callable(symbol_text)
-            else symbol_text.format(elem=element),
-            ha="center",
-            va="center",
-            transform=ax.transAxes,
-            **symbol_kwargs,
-        )
-
-        ax.axis("on")
-
-        if anno_kwds:
-            defaults = dict(
-                text=lambda plot_vals: si_fmt_int(len(plot_vals)),
-                xy=(0.8, 0.8),
-                xycoords="axes fraction",
-                fontsize=8,
-                horizontalalignment="center",
-                verticalalignment="center",
-            )
-            if callable(anno_kwds):
-                annotation = anno_kwds(plot_data)
-            else:
-                annotation = anno_kwds
-                anno_text = anno_kwds.get("text")
-                if isinstance(anno_text, dict):
-                    anno_text = anno_text.get(symbol)
-                elif callable(anno_text):
-                    anno_text = anno_text(plot_data)
-                annotation["text"] = anno_text
-            ax.annotate(**(defaults | annotation))
-
-        if plot_data is not None:
-            if callable(plot_kwds):
-                scatter_kwargs = plot_kwds(plot_data)
-            else:
-                scatter_kwargs = plot_kwds or {}
-
-            # Plot without colormap when data len is 2
-            if len(plot_data) == 2:
-                ax.plot(plot_data[0], plot_data[1], **(scatter_kwargs or {}))
-
-            # Plot with colormap when data len is 3
-            elif len(plot_data) == 3:
-                ax.plot(
-                    plot_data[0],
-                    plot_data[1],
-                    c=plot_data[2],
-                    cmap=cmap,
-                    **(scatter_kwargs or {}),
-                )
-
-            ax.tick_params(labelsize=8, direction="out")
-            if ax_kwds:
-                ax.set(**ax_kwds(plot_data) if callable(ax_kwds) else ax_kwds)
-
-        # Disable ticks for elements without data
-        else:
-            ax.set_xticks([])
-            ax.set_yticks([])
-
-        # Hide right/top borders
-        for side in ("right", "top"):
-            ax.spines[side].set_visible(b=False)
-
-    # Add colorbar if data dimensionality is 3
-    if isinstance(cmap, Colormap) and {len(data) for data in data.values()} == {3}:
-        # Get the min/max values for norm calculation
-        third_lists = [data[2] for data in data.values()]
-        vmin = min(min(third_list) for third_list in third_lists)
-        vmax = max(max(third_list) for third_list in third_lists)
-
-        norm = Normalize(vmin=vmin, vmax=vmax)
-
-        cbar_ax = fig.add_axes(cbar_coords)
-
-        fig.colorbar(
-            plt.cm.ScalarMappable(norm=norm, cmap=cmap),
-            cax=cbar_ax,
-            **{"orientation": "horizontal"} | (cbar_kwds or {}),
-        )
-
-        # Set colorbar title
-        cbar_title_kwds = cbar_title_kwds or {}
-        cbar_title_kwds.setdefault("fontsize", 12)
-        cbar_title_kwds.setdefault("pad", 10)
-        cbar_title_kwds["label"] = cbar_title
-        cbar_ax.set_title(**cbar_title_kwds)
-
-    return fig
+    return plotter.fig
 
 
 def ptable_splits(
@@ -1177,15 +1053,17 @@ def ptable_splits(
     cbar_title_kwds: dict[str, Any] | None = None,
     cbar_kwds: dict[str, Any] | None = None,
     on_empty: Literal["hide", "show"] = "hide",
-    **kwargs: Any,
 ) -> plt.Figure:
+    """Plot
+    TODO: add docstring
+    """
     plotter = PTableProjector(
         colormap=colormap,
         data=data,
-        # kwargs=plot_kwds,
+        # **plot_kwds,  # TODO
     )
 
-    # Call child plotter
+    # Call child plotter: evenly split rectangle
     child_args = {
         "start_angle": start_angle,
         "cmap": plotter.cmap,
