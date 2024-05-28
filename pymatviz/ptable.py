f0e63e0d21a077c10316ec3cd6b784a94326ebdf
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-05-28 09:35:37 +0800

    separate data preprocess to module

--- pymatviz/ptable.py
+++ pymatviz/ptable.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import itertools
 import math
 import warnings
-from collections.abc import Iterable, Sequence
+from collections.abc import Sequence
 from functools import partial
 from typing import TYPE_CHECKING, Literal, Union, get_args
 
@@ -20,6 +20,11 @@ from matplotlib.patches import Rectangle
 from pandas.api.types import is_numeric_dtype, is_string_dtype
 from pymatgen.core import Composition, Element
 
+from pymatviz._data import (
+    SupportedDataType,
+    SupportedValueType,
+    ptable_data_preprocessor,
+)
 from pymatviz.enums import Key
 from pymatviz.utils import ELEM_TYPE_COLORS, df_ptable, pick_bw_for_contrast, si_fmt
 
@@ -29,14 +34,6 @@ if TYPE_CHECKING:
 
     import plotly.graph_objects as go
 
-
-# Data types used internally by ptable plotters
-SupportedValueType = Union[Sequence[float], np.ndarray]
-
-# Data types that can be passed to PTableProjector and data_preprocessor
-SupportedDataType = Union[
-    dict[str, Union[float, Sequence[float], np.ndarray]], pd.DataFrame, pd.Series
-]
 
 CountMode = Literal[
     Key.composition, "fractional_composition", "reduced_composition", "occurrence"
@@ -155,188 +152,6 @@ def count_elements(
     return srs
 
 
-def data_preprocessor(data: SupportedDataType) -> pd.DataFrame:
-    """Preprocess input data for ptable plotters, including:
-        - Convert all data types to pd.DataFrame.
-        - Impute missing values.
-        - Handle anomalies such as NaN, infinity.
-        - Write vmin/vmax as metadata into the DataFrame.
-
-    Returns:
-        pd.DataFrame: The preprocessed DataFrame with element names
-            as index and values as columns.
-
-    Example:
-        >>> data_dict: dict = {
-            "H": 1.0,
-            "He": [2.0, 4.0],
-            "Li": [[6.0, 8.0], [10.0, 12.0]],
-        }
-
-        OR
-        >>> data_df: pd.DataFrame = pd.DataFrame(
-            data_dict.items(),
-            columns=["element", "heat_val"]
-            ).set_index("element")
-
-        OR
-        >>> data_series: pd.Series = pd.Series(data_dict)
-
-        >>> preprocess_data(data_dict / df / series)
-
-        # TODO: the following example looks incorrect, element
-        should be the index instead of a column.
-        Maybe support both, as it appears pass element as a
-        column (without setting it as index) is easier
-        What is the default behavior when pd.from_csv()?
-
-             Element   Value
-        0    H         [1.0, ]
-        1    He        [2.0, 4.0]
-        2    Li        [[6.0, 8.0], [10.0, 12.0]]
-
-        Metadata:
-            vmin: 1.0
-            vmax: 12.0
-    """
-
-    def format_pd_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-        """Fix pd.DataFrame that does not meet expected format."""
-
-        def fix_df_elem_as_col(df: pd.DataFrame) -> pd.DataFrame:
-            """Fix pd.DataFrame where elements are in a single column."""
-            # Copy and reset index to move element names to a column
-            new_df = df.copy()
-            new_df = new_df.reset_index()
-
-            # Find the column with element names
-            elem_col_name = None
-            for col in new_df.columns:
-                if set(new_df[col]).issubset(set(map(str, Element))):
-                    elem_col_name = col
-                    break
-
-            # Fix failed: cannot find the elements column
-            if elem_col_name is None:
-                return None
-
-            # Rename the column with elements and set it as index
-            new_df = new_df.rename(columns={elem_col_name: Key.element})
-            new_df = new_df.set_index(Key.element)
-
-            # Zip the remaining values into a single column
-            value_cols = [
-                col_name for col_name in new_df.columns if col_name != Key.element
-            ]
-            new_df[Key.heat_val] = new_df[value_cols].apply(list, axis=1)
-
-            # Drop the old value columns
-            return new_df[[Key.heat_val]]
-
-        # Check if input DataFrame is in expected format
-        if Key.element == df.index.name and Key.heat_val in df.columns:
-            return df
-
-        # Re-format it to expected
-        warnings.warn(
-            "pd.DataFrame has unexpected format, trying to fix.", stacklevel=2
-        )
-
-        # Try to search for elements as a column
-        fixed_df = fix_df_elem_as_col(df)
-        if fixed_df is not None:
-            return fixed_df
-
-        # Try to search for elements as a row
-        fixed_df = fix_df_elem_as_col(df.transpose())
-        if fixed_df is not None:
-            return fixed_df
-
-        raise RuntimeError("Cannot fix provided DataFrame.")
-
-    def handle_missing_and_infinity(
-        df: pd.DataFrame,
-        # missing_strategy: Literal["zero", "mean"] = "mean",
-    ) -> pd.DataFrame:
-        """Handle missing value (NaN) and infinity.
-
-        Infinity would be replaced by vmax(∞) or vmin(-∞).
-        Missing value would be replaced by selected strategy:
-            - zero: replace with zero
-            - mean: replace with mean value
-        """
-
-        def check_for_missing_inf(data: pd.Series) -> tuple[bool, bool]:
-            """Check if there is NaN or infinity in pandas Series.
-
-            Returns:
-                tuple[bool, bool]: Has NaN, has infinity.
-            """
-            # Check if there is missing value or infinity
-            all_values = data[Key.heat_val].explode().explode().explode()
-
-            # Convert to numeric, forcing non-numeric types to NaN
-            all_values = pd.to_numeric(all_values, errors="coerce")
-
-            has_nan = False
-            has_inf = False
-            # Check for NaN
-            if all_values.isna().to_numpy().any():
-                warnings.warn("NaN found in data.", stacklevel=2)
-                has_nan = True
-
-            # Check for infinity
-            if np.isinf(all_values).to_numpy().any():
-                warnings.warn("Infinity found in data.", stacklevel=2)
-                has_inf = True
-
-            return has_nan, has_inf
-
-        # Check for NaN and infinity
-        has_nan, has_inf = check_for_missing_inf(df)
-
-        return df
-
-    # Check and handle different supported data types
-    if isinstance(data, pd.DataFrame):
-        data_df = format_pd_dataframe(data)
-
-    elif isinstance(data, pd.Series):
-        data_df = data.to_frame(name=Key.heat_val)
-        data_df.index.name = Key.element
-
-    elif isinstance(data, dict):
-        data_df = pd.DataFrame(
-            data.items(), columns=[Key.element, Key.heat_val]
-        ).set_index(Key.element)
-
-    else:
-        type_name = type(data).__name__  # line too long
-        raise TypeError(
-            f"{type_name} unsupported, choose from {get_args(SupportedDataType)}"
-        )
-
-    # Convert all values to 1D np.array
-    data_df[Key.heat_val] = [
-        # String is Iterable too so would be converted to list of chars
-        # but users shouldn't pass strings anyway
-        np.array(list(val) if isinstance(val, Iterable) else [val])
-        for val in data_df[Key.heat_val]
-    ]
-
-    # Handle missing and anomalous values
-    data_df = handle_missing_and_infinity(data_df)
-
-    # Flatten up to triple nested lists
-    values = data_df[Key.heat_val].explode().explode().explode()
-    numeric_values = pd.to_numeric(values, errors="coerce")
-
-    # Write vmin/vmax into df.attrs for colorbar
-    data_df.attrs["vmin"] = numeric_values.min()  # ignores NaNs
-    data_df.attrs["vmax"] = numeric_values.max()
-    return data_df
-
-
 class PTableProjector:
     """Project (nest) a custom plot into a periodic table.
 
@@ -426,7 +241,7 @@ class PTableProjector:
     def data(self, data: SupportedDataType) -> None:
         """Set and preprocess the data. Also set normalizer."""
         # Preprocess data
-        self._data: pd.DataFrame = data_preprocessor(data)
+        self._data: pd.DataFrame = ptable_data_preprocessor(data)
 
         # Normalize data for colorbar
         self._norm: Normalize = Normalize(
--- tests/test_ptable.py
+++ tests/test_ptable.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import random
 import re
-from typing import TYPE_CHECKING, Any, Literal, get_args
+from typing import TYPE_CHECKING, Any, Literal
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -10,7 +10,6 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import pytest
-from numpy.testing import assert_allclose
 from plotly.exceptions import PlotlyError
 from pymatgen.core.periodic_table import Element
 
@@ -25,7 +24,7 @@ from pymatviz import (
     ptable_scatters,
 )
 from pymatviz.enums import Key
-from pymatviz.ptable import PTableProjector, SupportedDataType, data_preprocessor
+from pymatviz.ptable import PTableProjector
 from pymatviz.utils import df_ptable, si_fmt, si_fmt_int
 
 
@@ -36,111 +35,6 @@ if TYPE_CHECKING:
     from pymatgen.core import Composition
 
     from pymatviz.ptable import CountMode
-
-
-class TestDataPreprocessor:
-    test_dict: ClassVar = {
-        "H": 1,  # int
-        "He": [2.0, 4.0],  # float list
-        "Li": np.array([6.0, 8.0]),  # float array
-        "Na": 11.0,  # float
-        "Mg": {"a": -1, "b": 14.0}.values(),  # dict_values
-        "Al": {-1, 2.3},  # mixed int/float set
-    }
-
-    @staticmethod
-    def _validate_output_df(output_df: pd.DataFrame) -> None:
-        assert isinstance(output_df, pd.DataFrame)
-
-        assert list(output_df) == [Key.heat_val]
-        assert list(output_df.index) == ["H", "He", "Li", "Na", "Mg", "Al"]
-
-        assert_allclose(output_df.loc["H", Key.heat_val], [1.0])
-        assert_allclose(output_df.loc["He", Key.heat_val], [2.0, 4.0])
-        assert_allclose(output_df.loc["Li", Key.heat_val], [6.0, 8.0])
-        assert_allclose(output_df.loc["Na", Key.heat_val], [11.0])
-        assert_allclose(output_df.loc["Mg", Key.heat_val], [-1.0, 14.0])
-
-        assert output_df.attrs["vmin"] == -1.0
-        assert output_df.attrs["vmax"] == 14.0
-
-    def test_from_pd_dataframe(self) -> None:
-        input_df: pd.DataFrame = pd.DataFrame(
-            self.test_dict.items(), columns=[Key.element, Key.heat_val]
-        ).set_index(Key.element)
-
-        output_df: pd.DataFrame = data_preprocessor(input_df)
-
-        self._validate_output_df(output_df)
-
-    def test_from_bad_pd_dataframe(self) -> None:
-        """Test auto-fix of badly formatted pd.DataFrame."""
-        test_dict = {
-            "He": [2.0, 4.0],  # float list
-            "Li": np.array([6.0, 8.0]),  # float array
-            "Mg": {"a": -1, "b": 14.0}.values(),  # dict_values
-        }
-
-        input_df_0 = pd.DataFrame(test_dict)
-
-        # Elements as a row, and no proper row/column names
-        output_df_0 = data_preprocessor(input_df_0)
-
-        assert_allclose(output_df_0.loc["He", Key.heat_val], [2.0, 4.0])
-        assert_allclose(output_df_0.loc["Li", Key.heat_val], [6.0, 8.0])
-        assert_allclose(output_df_0.loc["Mg", Key.heat_val], [-1.0, 14.0])
-
-        # Elements as a column, and no proper row/column names
-        input_df_1 = input_df_0.copy().transpose()
-        output_df_1 = data_preprocessor(input_df_1)
-
-        assert_allclose(output_df_1.loc["He", Key.heat_val], [2.0, 4.0])
-        assert_allclose(output_df_1.loc["Li", Key.heat_val], [6.0, 8.0])
-        assert_allclose(output_df_1.loc["Mg", Key.heat_val], [-1.0, 14.0])
-
-    def test_from_pd_series(self) -> None:
-        input_series: pd.Series = pd.Series(self.test_dict)
-
-        output_df = data_preprocessor(input_series)
-
-        self._validate_output_df(output_df)
-
-    def test_from_dict(self) -> None:
-        input_dict = self.test_dict
-
-        output_df = data_preprocessor(input_dict)
-
-        self._validate_output_df(output_df)
-
-    def test_unsupported_type(self) -> None:
-        for invalid_data in ([0, 1, 2], range(5), "test", None):
-            err_msg = (
-                f"{type(invalid_data).__name__} unsupported, "
-                f"choose from {get_args(SupportedDataType)}"
-            )
-            with pytest.raises(TypeError, match=re.escape(err_msg)):
-                data_preprocessor(invalid_data)
-
-    def test_get_vmin_vmax(self) -> None:
-        # Test without nested list/array
-        test_dict_0 = {"H": 1, "He": [2, 4], "Li": np.array([6, 8])}
-
-        output_df_0 = data_preprocessor(test_dict_0)
-
-        assert output_df_0.attrs["vmin"] == 1
-        assert output_df_0.attrs["vmax"] == 8
-
-        # Test with nested list/array
-        test_dict_1 = {
-            "H": 1,
-            "He": [[2, 3], [4, 5]],
-            "Li": [np.array([6, 7]), np.array([8, 9])],
-        }
-
-        output_df_1 = data_preprocessor(test_dict_1)
-
-        assert output_df_1.attrs["vmin"] == 1
-        assert output_df_1.attrs["vmax"] == 9
 
 
 class TestPTableProjector:
@@ -203,15 +97,6 @@ class TestPTableProjector:
 
         assert legend.get_title().get_text() == legend_title
         assert legend.get_texts()[0].get_fontsize() == 12
-
-
-class TestMissingAnomalyHandle:
-    # TODO: finish this unit test
-    def test_handle_missing(self) -> None:
-        pass
-
-    def test_handle_infinity(self) -> None:
-        pass
 
 
 @pytest.fixture()
