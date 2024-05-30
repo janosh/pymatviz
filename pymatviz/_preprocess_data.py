2280595fba47810aaba28d98ae08d649eefe7d19
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-05-30 20:24:17 +0800

    Handle missing value (NaN) and infinity for ptable data (#152)
    * tweak comment location
    
    * add NaN and inf check func
    
    * add TODO tag
    
    * improve TODO tag
    
    * separate data preprocess to module
    
    * rename to preprocess_ptable_data
    
    * add test for `check_for_missing_inf`
    
    * add `get_df_nest_level` and tests
    
    * pack ax.ticks call kwargs
    
    * remove DEBUG tag
    
    * separate handle_missing_and_infinity and write mean to metadata
    
    * name tweaks and update placeholders
    
    * finish nan and infinity replacement
    
    * suppress a mypy error
    
    * fix unit test
    
    * update docstring
    
    * add check for warnings
    
    * df->df_in
    
    move [build-system] below [distutils.bdist_wheel] in pyproject
    
    * fix mypy errors by allowing any arguments on child_plotter Callable
    
    https://docs.python.org/3/library/typing.html#annotating-callable-objects
    
    * rename _data module to _preprocess_data
    
    * a missing rename
    
    * nest level test add np.array
    
    * simplify nest level check with arr ndim
    
    * df.apply -> df.map
    
    * (need input) fix unit test
    
    * np.array -> np.asarray
    
    * use dict to remap value for readability
    
    * pandas axis=1 -> 'columns'
    
    * simplify replace_missing_and_infinity
    
    * fix doc str typos
    
    * change preprocess_ptable_data RuntimeError to ValueError and tweak err msg
    
    * fix unit test
    
    * add test for unfixable df
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add back strategy arg and improve docs
    
    * fix typo
    
    * address PR comments from
    
    https://github.com/janosh/pymatviz/pull/152#issuecomment-2138542076
    https://github.com/janosh/pymatviz/pull/152#discussion_r1619728535
    https://github.com/janosh/pymatviz/pull/152#discussion_r1619735955
    
    ---------
    
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

---
+++ pymatviz/_preprocess_data.py
@@ -0,0 +1,267 @@
+"""Utils for data preprocessing and related tasks."""
+
+from __future__ import annotations
+
+import warnings
+from collections.abc import Iterable, Sequence
+from typing import TYPE_CHECKING, Union, get_args
+
+import numpy as np
+import pandas as pd
+from pymatgen.core import Element
+
+from pymatviz.enums import Key
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+
+
+# Data types that can be passed to PTableProjector and normalized by data_preprocessor
+# to SupportedValueType
+SupportedDataType = Union[
+    dict[str, Union[float, Sequence[float], np.ndarray]], pd.DataFrame, pd.Series
+]
+
+# Data types used internally by ptable plotters (returned by preprocess_ptable_data)
+SupportedValueType = Union[Sequence[float], np.ndarray]
+
+
+def check_for_missing_inf(df_in: pd.DataFrame, col: str) -> tuple[bool, bool]:
+    """Check if there is NaN or infinity in a DataFrame column.
+
+    Args:
+        df_in (pd.DataFrame): DataFrame to check.
+        col (str): Name of the column to check.
+
+    Returns:
+        tuple[bool, bool]: Has NaN, has infinity.
+    """
+    # Check if there is missing value or infinity
+    all_values = df_in[col].explode().explode().explode()
+
+    # Convert to numeric, forcing non-numeric types to NaN
+    all_values = pd.to_numeric(all_values, errors="coerce")
+
+    # Check for NaN
+    has_nan = False
+    if all_values.isna().to_numpy().any():
+        warnings.warn("NaN found in data", stacklevel=2)
+        has_nan = True
+
+    # Check for infinity
+    has_inf = False
+    if np.isinf(all_values).to_numpy().any():
+        warnings.warn("Infinity found in data", stacklevel=2)
+        has_inf = True
+
+    return has_nan, has_inf
+
+
+def get_df_nest_level(df_in: pd.DataFrame, col: str) -> int:
+    """Check for maximum nest level in a DataFrame column.
+
+    Definition of nest level:
+        Level 0 (no list):       "Fe": 1
+        Level 1 (flat list):     "Co": [1, 2]
+        Level 2 (nested lists):  "Ni": [[1, 2], [3, 4], ]
+        ...
+
+    Args:
+        df_in (pd.DataFrame): The DataFrame to check.
+        col (str): Name of the column to check.
+
+    Returns:
+        int: The maximum nest level.
+    """
+    return df_in[col].map(lambda val: np.asarray(val).ndim).max()
+
+
+def replace_missing_and_infinity(
+    df_in: pd.DataFrame,
+    col: str,
+    missing_strategy: Literal["zero", "mean"] = "mean",
+) -> pd.DataFrame:
+    """Replace missing value (NaN) and infinity.
+
+    Infinity would be replaced by vmax(∞) or vmin(-∞).
+    Missing value would be replaced by selected strategy:
+        - zero: replace with zero
+        - mean: replace with mean value
+
+    Args:
+        df_in (DataFrame): DataFrame to process.
+        col (str): Name of the column to process.
+        missing_strategy: missing value replacement strategy.
+    """
+    # Check for NaN and infinity
+    has_nan, has_inf = check_for_missing_inf(df_in, Key.heat_val)
+
+    if not has_nan and not has_inf:
+        return df_in
+
+    # Can only handle nest level 1 at this moment
+    if (has_nan or has_inf) and get_df_nest_level(df_in, col) > 1:
+        raise RuntimeError("Unable to replace NaN and inf for nest level > 1")
+
+    # Get replacement value for missing and infinity
+    values = df_in[col].explode()
+    numeric_values = pd.to_numeric(values, errors="coerce")
+
+    replacement_nan = 0 if missing_strategy == "zero" else numeric_values.mean()
+    replacement_inf_pos = numeric_values[numeric_values != np.inf].max()
+    replacement_inf_neg = numeric_values[numeric_values != -np.inf].min()
+
+    replacements = {
+        np.nan: replacement_nan,
+        np.inf: replacement_inf_pos,
+        -np.inf: replacement_inf_neg,
+    }
+
+    df_in[col] = df_in[col].apply(
+        lambda val: replacements.get(val, val)
+        if isinstance(val, float)
+        else np.array([replacements.get(v, v) for v in val])
+    )
+
+    return df_in
+
+
+def preprocess_ptable_data(
+    data: SupportedDataType,
+    missing_strategy: Literal["zero", "mean"] = "mean",
+) -> pd.DataFrame:
+    """Preprocess input data for ptable plotters, including:
+        - Convert all data types to pd.DataFrame.
+        - Replace missing values (NaN) by selected strategy.
+        - Replace infinities with vmax(∞) or vmin(-∞).
+        - Write vmin/mean/vmax as metadata into the DataFrame.
+
+    Args:
+        data (dict[str, float | Sequence[float]] | pd.DataFrame | pd.Series):
+            Input data to preprocess.
+        missing_strategy: missing value replacement strategy.
+
+    Returns:
+        pd.DataFrame: The preprocessed DataFrame with element names
+            as index and values as columns.
+
+    Example:
+        >>> data_dict: dict = {
+            "H": 1.0,
+            "He": [2.0, 4.0],
+            "Li": [[6.0, 8.0], [10.0, 12.0]],
+        }
+
+        OR
+        >>> data_df: pd.DataFrame = pd.DataFrame(
+            data_dict.items(),
+            columns=["element", "heat_val"]
+            ).set_index("element")
+
+        OR
+        >>> data_series: pd.Series = pd.Series(data_dict)
+
+        >>> preprocess_data(data_dict / df / series)
+
+        >>> data_df
+            Element   Value
+            H         [1.0, ]
+            He        [2.0, 4.0]
+            Li        [[6.0, 8.0], [10.0, 11.0]]
+
+        Metadata (data_df.attrs):
+            vmin: 1.0
+            mean: 6.0
+            vmax: 11.0
+    """
+
+    def format_pd_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+        """Format pd.DataFrame that may not meet expected format."""
+
+        def fix_df_elem_as_col(df: pd.DataFrame) -> pd.DataFrame:
+            """Fix pd.DataFrame where elements are in a single column."""
+            # Copy and reset index to move element names to a column
+            new_df = df.copy().reset_index()
+
+            # Find the column with element names
+            elem_col_name = None
+            for col in new_df.columns:
+                if set(new_df[col]).issubset(set(map(str, Element))):
+                    elem_col_name = col
+                    break
+
+            # Fix failed: cannot find the elements column
+            if elem_col_name is None:
+                return None
+
+            # Rename the column with elements and set it as index
+            new_df = new_df.rename(columns={elem_col_name: Key.element})
+            new_df = new_df.set_index(Key.element)
+
+            # Zip the remaining values into a single column
+            value_cols = [
+                col_name for col_name in new_df.columns if col_name != Key.element
+            ]
+            new_df[Key.heat_val] = new_df[value_cols].apply(list, axis="columns")
+
+            # Drop the old value columns
+            return new_df[[Key.heat_val]]
+
+        # Check if input DataFrame is in expected format
+        if Key.element == df.index.name and Key.heat_val in df.columns:
+            return df
+
+        # Re-format it to expected
+        warnings.warn("DataFrame has unexpected format, trying to fix.", stacklevel=2)
+
+        # Try to search for elements as a column
+        fixed_df = fix_df_elem_as_col(df)
+        if fixed_df is not None:
+            return fixed_df
+
+        # Try to search for elements as a row
+        fixed_df = fix_df_elem_as_col(df.transpose())
+        if fixed_df is not None:
+            return fixed_df
+
+        raise KeyError(
+            f"Cannot handle dataframe={df}, needs row/column named "
+            f"{Key.element} and {Key.heat_val}"
+        )
+
+    # Convert supported data types to DataFrame
+    if isinstance(data, pd.DataFrame):
+        data_df = format_pd_dataframe(data)
+
+    elif isinstance(data, pd.Series):
+        data_df = data.to_frame(name=Key.heat_val)
+        data_df.index.name = Key.element
+    elif isinstance(data, dict):
+        data_df = pd.DataFrame(
+            data.items(), columns=[Key.element, Key.heat_val]
+        ).set_index(Key.element)
+    else:
+        type_name = type(data).__name__
+        raise TypeError(
+            f"{type_name} unsupported, choose from {get_args(SupportedDataType)}"
+        )
+
+    data_df[Key.heat_val] = data_df[Key.heat_val].apply(
+        lambda val: np.array(list(val))
+        if isinstance(val, Iterable) and not isinstance(val, str)
+        else np.array([val])
+    )
+
+    data_df = replace_missing_and_infinity(
+        data_df, col=Key.heat_val, missing_strategy=missing_strategy
+    )
+
+    numeric_values = pd.to_numeric(
+        data_df[Key.heat_val].explode().explode().explode(), errors="coerce"
+    )
+    data_df.attrs["vmin"] = numeric_values.min()
+    data_df.attrs["mean"] = numeric_values.mean()
+    data_df.attrs["vmax"] = numeric_values.max()
+
+    return data_df
---
+++ tests/test_preprocess_data.py
@@ -0,0 +1,304 @@
+from __future__ import annotations
+
+import re
+from typing import TYPE_CHECKING, get_args
+
+import numpy as np
+import pandas as pd
+import pytest
+from numpy.testing import assert_allclose
+
+from pymatviz._preprocess_data import (
+    SupportedDataType,
+    check_for_missing_inf,
+    get_df_nest_level,
+    preprocess_ptable_data,
+    replace_missing_and_infinity,
+)
+from pymatviz.enums import Key
+
+
+if TYPE_CHECKING:
+    from typing import ClassVar
+
+
+class TestPreprocessPtableData:
+    test_dict: ClassVar = {
+        "H": 1,  # int
+        "He": [2.0, 4.0],  # float list
+        "Li": np.array([6.0, 8.0]),  # float array
+        "Na": 11.0,  # float
+        "Mg": {"a": -1, "b": 14.0}.values(),  # dict_values
+        "Al": {-1, 2.3},  # mixed int/float set
+    }
+
+    @staticmethod
+    def _validate_output_df(output_df: pd.DataFrame) -> None:
+        assert isinstance(output_df, pd.DataFrame)
+
+        assert list(output_df) == [Key.heat_val]
+        assert list(output_df.index) == ["H", "He", "Li", "Na", "Mg", "Al"]
+
+        assert_allclose(output_df.loc["H", Key.heat_val], [1.0])
+        assert_allclose(output_df.loc["He", Key.heat_val], [2.0, 4.0])
+        assert_allclose(output_df.loc["Li", Key.heat_val], [6.0, 8.0])
+        assert_allclose(output_df.loc["Na", Key.heat_val], [11.0])
+        assert_allclose(output_df.loc["Mg", Key.heat_val], [-1.0, 14.0])
+
+        assert output_df.attrs["vmin"] == -1.0
+        assert output_df.attrs["mean"] == 4.63
+        assert output_df.attrs["vmax"] == 14.0
+
+    def test_from_pd_dataframe(self) -> None:
+        input_df: pd.DataFrame = pd.DataFrame(
+            self.test_dict.items(), columns=[Key.element, Key.heat_val]
+        ).set_index(Key.element)
+
+        output_df: pd.DataFrame = preprocess_ptable_data(input_df)
+
+        self._validate_output_df(output_df)
+
+    def test_from_bad_pd_dataframe(self) -> None:
+        """Test auto-fix of badly formatted pd.DataFrame."""
+        test_dict = {
+            "He": [2.0, 4.0],  # float list
+            "Li": np.array([6.0, 8.0]),  # float array
+            "Mg": {"a": -1, "b": 14.0}.values(),  # dict_values
+        }
+
+        df_in = pd.DataFrame(test_dict)
+
+        # Elements as a row, and no proper row/column names
+        df_out = preprocess_ptable_data(df_in)
+
+        assert_allclose(df_out.loc["He", Key.heat_val], [2.0, 4.0])
+        assert_allclose(df_out.loc["Li", Key.heat_val], [6.0, 8.0])
+        assert_allclose(df_out.loc["Mg", Key.heat_val], [-1.0, 14.0])
+
+        # Elements as a column, and no proper row/column names
+        df_in_transp = pd.DataFrame(test_dict).transpose()
+        df_out_transp = preprocess_ptable_data(df_in_transp)
+
+        assert_allclose(df_out_transp.loc["He", Key.heat_val], [2.0, 4.0])
+        assert_allclose(df_out_transp.loc["Li", Key.heat_val], [6.0, 8.0])
+        assert_allclose(df_out_transp.loc["Mg", Key.heat_val], [-1.0, 14.0])
+
+    def test_df_cannot_fix(self) -> None:
+        # No complete elements column/row
+        df_without_complete_elem = pd.DataFrame(
+            {
+                "Hello": [2.0, 4.0],  # Not an element
+                "Li": np.array([6.0, 8.0]),
+                "Mg": {"a": -1, "b": 14.0}.values(),
+            }
+        )
+        with pytest.raises(KeyError, match="Cannot handle dataframe="):
+            preprocess_ptable_data(df_without_complete_elem)
+
+    def test_from_pd_series(self) -> None:
+        input_series: pd.Series = pd.Series(self.test_dict)
+
+        output_df = preprocess_ptable_data(input_series)
+
+        self._validate_output_df(output_df)
+
+    def test_from_dict(self) -> None:
+        input_dict = self.test_dict
+
+        output_df = preprocess_ptable_data(input_dict)
+
+        self._validate_output_df(output_df)
+
+    def test_unsupported_type(self) -> None:
+        for invalid_data in ([0, 1, 2], range(5), "test", None):
+            err_msg = (
+                f"{type(invalid_data).__name__} unsupported, "
+                f"choose from {get_args(SupportedDataType)}"
+            )
+            with pytest.raises(TypeError, match=re.escape(err_msg)):
+                preprocess_ptable_data(invalid_data)
+
+    def test_get_vmin_vmax(self) -> None:
+        # Test without nested list/array
+        test_dict_0 = {"H": 1, "He": [2, 4], "Li": np.array([6, 8])}
+
+        output_df_0 = preprocess_ptable_data(test_dict_0)
+
+        assert output_df_0.attrs["vmin"] == 1
+        assert output_df_0.attrs["vmax"] == 8
+
+        # Test with nested list/array
+        test_dict_1 = {
+            "H": 1,
+            "He": [[2, 3], [4, 5]],
+            "Li": [np.array([6, 7]), np.array([8, 9])],
+        }
+
+        output_df_1 = preprocess_ptable_data(test_dict_1)
+
+        assert output_df_1.attrs["vmin"] == 1
+        assert output_df_1.attrs["vmax"] == 9
+
+
+def test_check_for_missing_inf() -> None:
+    # Test a normal DataFrame
+    normal_df = pd.DataFrame(
+        {
+            "Fe": [1, 2, 3],
+            "O": [4, 5, 6],
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert check_for_missing_inf(normal_df, Key.heat_val) == (False, False)
+
+    # Test DataFrame with missing value (NaN)
+    df_with_missing = pd.DataFrame(
+        {
+            "Fe": [1, 2, np.nan],
+            "O": [4, 5, 6],
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert check_for_missing_inf(df_with_missing, Key.heat_val) == (True, False)
+
+    # Test DataFrame with infinity
+    df_with_inf = pd.DataFrame(
+        {
+            "Fe": [1, 2, np.inf],
+            "O": [4, 5, 6],
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert check_for_missing_inf(df_with_inf, Key.heat_val) == (False, True)
+
+    # Test DataFrame with missing value (NaN) and infinity
+    df_with_nan_inf = pd.DataFrame(
+        {
+            "Fe": [1, 2, np.inf],
+            "O": [4, 5, np.nan],
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert check_for_missing_inf(df_with_nan_inf, Key.heat_val) == (True, True)
+
+
+def test_get_df_nest_level() -> None:
+    # Test nest level 0
+    df_level_0 = pd.DataFrame(
+        {
+            "Fe": 1,
+            "O": 2,
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert get_df_nest_level(df_level_0, Key.heat_val) == 0
+
+    # Test nest level 1
+    df_level_1 = pd.DataFrame(
+        {
+            "Fe": [1, 2, 3],
+            "O": [4, 5, 6],
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert get_df_nest_level(df_level_1, Key.heat_val) == 1
+
+    df_level_1_arr = pd.DataFrame(
+        {
+            "Fe": 1,
+            "O": np.array([4, 5, 6]),
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert get_df_nest_level(df_level_1_arr, Key.heat_val) == 1
+
+    # Test nest level 2
+    df_level_2 = pd.DataFrame(
+        {
+            "Fe": [1, 2, 3],
+            "O": [[4, 5], [6, 7]],  # get max level
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert get_df_nest_level(df_level_2, Key.heat_val) == 2
+
+    df_level_2_arr = pd.DataFrame(
+        {
+            "Fe": [1, 2, 3],
+            "O": np.array([[4, 5], [6, 7]]),  # get max level
+        }.items(),
+        columns=[Key.element, Key.heat_val],
+    ).set_index(Key.element)
+
+    assert get_df_nest_level(df_level_2_arr, Key.heat_val) == 2
+
+
+class TestReplaceMissingAndInfinity:
+    def test_replace_missing(self) -> None:
+        df_with_nan = pd.DataFrame(
+            {
+                "Fe": [1, 2, 3],
+                "O": [4, 5, np.nan],
+            }.items(),
+            columns=[Key.element, Key.heat_val],
+        ).set_index(Key.element)
+
+        # Test missing strategy: mean (default)
+        with pytest.warns(match="NaN found in data"):
+            processed_df_mean = replace_missing_and_infinity(
+                df_with_nan.copy(), Key.heat_val
+            )
+        assert_allclose(processed_df_mean.loc["O", Key.heat_val], [4, 5, 3])
+
+        # Test missing strategy: zero
+        with pytest.warns(match="NaN found in data"):
+            processed_df_zero = replace_missing_and_infinity(
+                df_with_nan.copy(), Key.heat_val, "zero"
+            )
+        assert_allclose(processed_df_zero.loc["O", Key.heat_val], [4, 5, 0])
+
+    def test_replace_infinity(self) -> None:
+        df_with_inf = pd.DataFrame(
+            {
+                "Fe": [1, 2, np.inf],
+                "O": [4, 5, 6],
+            }.items(),
+            columns=[Key.element, Key.heat_val],
+        ).set_index(Key.element)
+
+        with pytest.warns(match="Infinity found in data"):
+            processed_df = replace_missing_and_infinity(df_with_inf, Key.heat_val)
+        assert_allclose(processed_df.loc["Fe", Key.heat_val], [1, 2, 6])
+
+    def test_replace_both(self) -> None:
+        df_with_both = pd.DataFrame(
+            {
+                "Fe": [1, 2, np.inf],
+                "O": [4, 5, np.nan],
+            }.items(),
+            columns=[Key.element, Key.heat_val],
+        ).set_index(Key.element)
+
+        processed_df = replace_missing_and_infinity(df_with_both, Key.heat_val, "zero")
+        assert_allclose(processed_df.loc["Fe", Key.heat_val], [1, 2, 5])
+        assert_allclose(processed_df.loc["O", Key.heat_val], [4, 5, 0])
+
+    def test_too_deep_nest(self) -> None:
+        df_level_2 = pd.DataFrame(
+            {
+                "Fe": [1, 2, 3],
+                "O": [[4, 5], [6, np.nan]],
+            }.items(),
+            columns=[Key.element, Key.heat_val],
+        ).set_index(Key.element)
+
+        with pytest.raises(RuntimeError, match="Unable to replace NaN and inf"):
+            replace_missing_and_infinity(df_level_2, col=Key.heat_val)
