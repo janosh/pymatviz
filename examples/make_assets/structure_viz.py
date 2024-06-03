82e07fa0d419b971694830e08b98d6d1b2881ad5
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-06-03 12:57:51 -0400

    Support passing sequence of structures to `plot_structure_2d()` to be plotted in grid (#156)
    * bump pre-commit hooks
    
    * support passing sequence of structures to plot_structure_2d to be plotted in grid
    
    supports list, tuple, dict, pd.Series
    uses keys, structure.properties['id'] or structure.formula as subplot titles in that order
    
    * add test_plot_structure_2d_multiple
    
    * update all multi-structure call sites of plot_structure_2d()

--- examples/make_assets/structure_viz.py
+++ examples/make_assets/structure_viz.py
@@ -1,4 +1,6 @@
 # %%
+from typing import cast
+
 import matplotlib.pyplot as plt
 from matminer.datasets import load_dataset
 from mp_api.client import MPRester
@@ -16,25 +18,14 @@ df_phonons = load_dataset("matbench_phonons")
 
 
 # %% Plot Matbench phonon structures
-n_rows, n_cols = 3, 4
-fig, axs = plt.subplots(n_rows, n_cols, figsize=(3 * n_cols, 3 * n_rows))
-title = f"{len(axs.flat)} Matbench phonon structures"
+n_structs = 12
+fig, axs = plot_structure_2d(
+    df_phonons[Key.structure].iloc[:n_structs],
+    show_bonds=True,
+    bond_kwargs=dict(facecolor="gray", linewidth=2, linestyle="dotted"),
+)
+title = f"{n_structs} Matbench phonon structures"
 fig.suptitle(title, fontweight="bold", fontsize=20)
-
-for idx, (row, ax) in enumerate(zip(df_phonons.itertuples(), axs.flat), start=1):
-    struct = row[Key.structure]
-    spg_num = struct.get_space_group_info()[1]
-    struct.add_oxidation_state_by_guess()
-
-    plot_structure_2d(
-        struct,
-        ax=ax,
-        show_bonds=True,
-        bond_kwargs=dict(facecolor="gray", linewidth=2, linestyle="dotted"),
-    )
-    sub_title = f"{idx + 1}. {struct.formula} ({spg_num})"
-    ax.set_title(sub_title, fontweight="bold")
-
 fig.show()
 save_and_compress_svg(fig, "matbench-phonons-structures-2d")
 
@@ -52,7 +43,7 @@ for mp_id, struct in disordered_structs.items():
         elif "Zr" in site.species:
             site.species = {"Zr": 0.5, "Hf": 0.5}
 
-    ax = plot_structure_2d(struct)
+    ax = cast(plt.Axes, plot_structure_2d(struct))
     _, spacegroup = struct.get_space_group_info()
 
     formula = struct.formula.replace(" ", "")
