61a92d8917980577f97d318fe52a02925fd9d6aa
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-06-11 12:03:06 -0400

    Fix `ptable_heatmap_ratio` legend (#158)
    * fix ptable_heatmap_ratio legend
    
    * update readme svg of ptable ratio heatmap
    
    * add test to catch future regressions
    
    * compress new SVG assets

--- tests/test_ptable.py
+++ tests/test_ptable.py
@@ -328,8 +328,27 @@ def test_ptable_heatmap_ratio(
     glass_elem_counts: pd.Series[int],
 ) -> None:
     # composition strings
-    ax = ptable_heatmap_ratio(glass_formulas, steel_formulas)
+    not_in_numerator = ("#eff", "gray: not in 1st list")
+    not_in_denominator = ("lightskyblue", "blue: not in 2nd list")
+    not_in_either = ("white", "white: not in either")
+    ax = ptable_heatmap_ratio(
+        glass_formulas,
+        steel_formulas,
+        not_in_numerator=not_in_numerator,
+        not_in_denominator=not_in_denominator,
+        not_in_either=not_in_either,
+    )
     assert isinstance(ax, plt.Axes)
+
+    # check presence of legend handles 'not in numerator' and 'not in denominator'
+    legend = ax.get_legend()
+    assert legend is None
+    # get text annotations
+    texts = ax.texts
+    assert len(texts) == 239
+    all_texts = [txt.get_text() for txt in texts]
+    for not_in in (not_in_numerator, not_in_denominator, not_in_either):
+        assert not_in[1] in all_texts
 
     # element counts
     ptable_heatmap_ratio(glass_elem_counts, steel_elem_counts, normalize=True)
