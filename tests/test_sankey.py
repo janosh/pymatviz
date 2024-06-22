fc7092051a3fcf993b24c8fa96a3e8f790f78ce6
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-06-21 20:47:46 -0400

    Fix `ruff` `NPY002` (#162)
    * fix and unignore ruff NPY002
    
    * remove unused docs code
    
    * add ruff rule names

--- tests/test_sankey.py
+++ tests/test_sankey.py
@@ -2,18 +2,19 @@ from __future__ import annotations
 
 from typing import Literal
 
-import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import pytest
 
 from pymatviz.sankey import sankey_from_2_df_cols
+from tests.conftest import np_rng
 
 
 @pytest.mark.parametrize("labels_with_counts", [True, False, "percent"])
 def test_sankey_from_2_df_cols(labels_with_counts: bool | Literal["percent"]) -> None:
     col_names = ["col_a", "col_b"]
-    df_rand = pd.DataFrame(np.random.randint(0, 10, size=(100, 2)), columns=col_names)
+
+    df_rand = pd.DataFrame(np_rng.integers(0, 10, size=(100, 2)), columns=col_names)
     fig = sankey_from_2_df_cols(
         df_rand, col_names, labels_with_counts=labels_with_counts
     )
