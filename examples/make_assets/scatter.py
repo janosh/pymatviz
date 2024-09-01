265df85a1da0fed6dcd8dd7c18947c87e4dd48dd
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-09-01 05:07:05 +0000

    fix missing packages matminer kaleido in test-scripts SI

--- examples/make_assets/scatter.py
+++ examples/make_assets/scatter.py
@@ -28,7 +28,7 @@ rand_regression_size = 500
 np_rng = np.random.default_rng(seed=0)
 y_true = np_rng.normal(5, 4, rand_regression_size)
 y_pred = 1.2 * y_true - 2 * np_rng.normal(0, 1, rand_regression_size)
-y_std = (y_true - y_pred) * 10 * np_rng.normal(0, 0.1, rand_regression_size)
+y_std = abs((y_true - y_pred) * 10 * np_rng.normal(0, 0.1, rand_regression_size))
 
 
 # %% density scatter plotly
