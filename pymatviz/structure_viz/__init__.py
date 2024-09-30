d1d98136e8ea10751148225d76255f04dc3cfa38
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-09-30 15:26:50 +0800

    remove todo tag and add more future statement

--- pymatviz/structure_viz/__init__.py
+++ pymatviz/structure_viz/__init__.py
@@ -1,4 +1,6 @@
 """2D and 3D plots of Structures."""
 
+from __future__ import annotations
+
 from pymatviz.structure_viz.mpl import structure_2d
 from pymatviz.structure_viz.plotly import structure_2d_plotly, structure_3d_plotly
