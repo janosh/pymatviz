705ed16951ffb82e4150b2e4b9f39f726b7c613c
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-09-30 14:30:32 +0800

    finish the interactive checker

--- pymatviz/_tests.py
+++ pymatviz/_tests.py
@@ -8,12 +8,15 @@ import os
 import matplotlib.pyplot as plt
 
 
-def interactive_check(plot: plt.Figure) -> bool:
-    """Ask a human to visually inspect a plot.
+def interactive_check(
+    plot: plt.Figure | plt.Axes,
+    elem_to_check: str = "figure",
+) -> bool:
+    """Ask a human to visually inspect a matplotlib Figure or Axes.
 
     Args:
-        plot (plt.Figure): plot to inspect.
-        TODO: handle other types (Axis...)
+        plot (plt.Figure | plt.Axes): Plot to inspect (either a Figure or Axes).
+        elem_to_check (str): Prompt for what element in the figure to check.
 
     Returns:
         bool: whether this passes inspection.
@@ -22,5 +25,16 @@ def interactive_check(plot: plt.Figure) -> bool:
     if os.getenv("GITHUB_ACTIONS") == "true":
         return True
 
-    # placeholder
-    assert isinstance(plot, plt.Figure)
+    # Handle matplotlib Axes
+    if isinstance(plot, plt.Axes):
+        plot = plot.figure
+    elif not isinstance(plot, plt.Figure):
+        raise TypeError(f"plot {type(plot).__name__} not supported.")
+
+    plot.show()
+
+    return (
+        input(f"Please check the {elem_to_check}. Is this plot good? (y/n): ")
+        .strip()
+        .lower()
+    ) == "y"
