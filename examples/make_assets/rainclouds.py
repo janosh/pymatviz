4178741fc67906a743c42da483d106751391a3d6
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-10-08 11:00:18 -0400

    test_rainclouds.py

--- examples/make_assets/rainclouds.py
+++ examples/make_assets/rainclouds.py
@@ -20,16 +20,13 @@ data_bimodal = {
     ),
 }
 
-fig_bimodal = pmv.rainclouds(data_bimodal, figsize=(800, 400))
-fig_bimodal.update_layout(
-    title="Raincloud Plot: Two Bimodal Distributions",
-    xaxis_title="Value",
-    yaxis_title="Distribution",
-)
-fig_bimodal.layout.margin.t = 40
-fig_bimodal.layout.title.x = 0.5
-fig_bimodal.show()
-pmv.io.save_and_compress_svg(fig_bimodal, "raincloud-bimodal")
+fig_bi = pmv.rainclouds(data_bimodal)
+fig_bi.layout.title.update(text="Raincloud Plot: Two Bimodal Distributions", x=0.5)
+fig_bi.layout.xaxis.title = "Value"
+fig_bi.layout.yaxis.title = "Distribution"
+fig_bi.layout.margin.t = 40
+fig_bi.show()
+pmv.io.save_and_compress_svg(fig_bi, "raincloud-bimodal")
 
 
 # Example 2: Three trimodal distributions
@@ -57,13 +54,10 @@ data_trimodal = {
     ),
 }
 
-fig_trimodal = pmv.rainclouds(data_trimodal, figsize=(1000, 600))
-fig_trimodal.update_layout(
-    title="Raincloud Plot: Three Trimodal Distributions",
-    xaxis_title="Value",
-    yaxis_title="Distribution",
-)
-fig_trimodal.layout.margin.t = 40
-fig_trimodal.layout.title.x = 0.5
-fig_trimodal.show()
-pmv.io.save_and_compress_svg(fig_trimodal, "raincloud-trimodal")
+fig_tri = pmv.rainclouds(data_trimodal)
+fig_tri.layout.title.update(text="Raincloud Plot: Three Trimodal Distributions", x=0.5)
+fig_tri.layout.xaxis.title = "Value"
+fig_tri.layout.yaxis.title = "Distribution"
+fig_tri.layout.margin.t = 40
+fig_tri.show()
+pmv.io.save_and_compress_svg(fig_tri, "raincloud-trimodal")
