382bdfba0d0ad47fc89ef55c6392d4f05101b5e8
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-11-18 11:56:47 +0800

    copy random data generator

--- assets/asset_makers/scatter/density_scatter_plotly.py
+++ assets/asset_makers/scatter/density_scatter_plotly.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import matplotlib.pyplot as plt
-import numpy as np
 import plotly.express as px
 import plotly.io as pio
 
@@ -19,14 +18,6 @@ plt.rc("savefig", bbox="tight", dpi=200)
 plt.rc("axes", titlesize=16, titleweight="bold")
 plt.rc("figure", dpi=200, titlesize=20, titleweight="bold")
 plt.rcParams["figure.constrained_layout.use"] = True
-
-
-# Random regression data
-rand_regression_size = 500
-np_rng = np.random.default_rng(seed=0)
-y_true = np_rng.normal(5, 4, rand_regression_size)
-y_pred = 1.2 * y_true - 2 * np_rng.normal(0, 1, rand_regression_size)
-y_std = abs((y_true - y_pred) * 10 * np_rng.normal(0, 0.1, rand_regression_size))
 
 
 # %% density scatter plotly
