9338db71388c0f86e59eed5e8eb0ac9c12bbe1b2
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-11-18 07:03:28 -0500

    add tests/data/regression.py

--- pymatviz/data/regression.py
+++ pymatviz/data/regression.py
@@ -3,9 +3,9 @@
 import numpy as np
 
 
-_rand_regression_size = 500
+n_samples = 500
 np_rng = np.random.default_rng(seed=0)
 
-y_true = np_rng.normal(5, 4, _rand_regression_size)
-y_pred = 1.2 * y_true - 2 * np_rng.normal(0, 1, _rand_regression_size)
-y_std = (y_true - y_pred) * 10 * np_rng.normal(0, 0.1, _rand_regression_size)
+y_true = np_rng.normal(5, 4, n_samples)
+y_pred = 1.2 * y_true - 2 * np_rng.normal(0, 1, n_samples)
+y_std = (y_true - y_pred) * 10 * np_rng.normal(0, 0.1, n_samples)
