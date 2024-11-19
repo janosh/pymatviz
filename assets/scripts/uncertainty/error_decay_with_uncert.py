5f751d16cc9407129f4358d69d5c54d69a61e3fd
Haoyu (Daniel) YANG <yanghaoyu97@outlook.com>
2024-11-19 20:32:44 +0800

    Split `make_assets` scripts by plot functions (#247)
    * migrate some changes
    
    * put under dir first
    
    * split coordination
    
    * split histogram
    
    * split phonon
    
    * splig rdf
    
    * splig scatter
    
    * splig structure_viz and scatter
    
    * split the rest
    
    * split ptable
    
    * copy random data generator
    
    * rename powerups plotly
    
    * remove plotly example from ptable mpl
    
    * move assets/(asset_makers->scripts)
    
    * mv assets/*.svg assets/svg/*.svg
    
    * change save_and_compress_svg default output directory to assets/svg/
    
    * restore examples/ ruff per-file-ignores
    
    * rm assets/scripts/(scatter|uncertainty)/_random_regression_data.py
    
    unused, left from bad merge conflict resolve
    
    * revert ptable_heatmap_splits
    
    * add tests/data/regression.py
    
    * fix with_marginal_hist creating two nested coordinated axes in density_hexbin_(hex_)with_hist
    
    * make dummy data generator modular a bit
    
    * add config_matplotlib
    
    * remove np_rng
    
    * enhance description of dummy data with GPT
    
    * migrate ptable
    
    * migrate phonons
    
    * relocate ptable dash app
    
    * migrate the rest
    
    * mv pymatviz/test/data.py to pymatviz/data/__init__.py
    
    mv tests/test/test_data.py to tests/test_data.py
    
    * fix test.yml find-scripts looking in wrong folder
    
    * merge pymatviz/test/config.py into pymatviz/utils.py and rename config_matplotlib to apply_matplotlib_template
    
    add unit test for apply_matplotlib_template()
    remove unneeded plotly templates from matplotlib asset scripts
    
    * fix asset scripts failing in CI, delete duplicate assets/scripts/powerups/powerups.py
    
    remove unused pmv.set_plotly_template
    
    ---------
    
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

---
+++ assets/scripts/uncertainty/error_decay_with_uncert.py
@@ -0,0 +1,22 @@
+# %%
+import numpy as np
+
+import pymatviz as pmv
+
+
+np_rng = np.random.default_rng(seed=0)
+dummy_data = pmv.data.regression()
+
+
+# %% Uncertainty Plots
+ax = pmv.error_decay_with_uncert(dummy_data.y_true, dummy_data.y_pred, dummy_data.y_std)
+pmv.io.save_and_compress_svg(ax, "error-decay-with-uncert")
+
+eps = 0.2 * np_rng.standard_normal(*dummy_data.y_std.shape)
+
+ax = pmv.error_decay_with_uncert(
+    dummy_data.y_true,
+    dummy_data.y_pred,
+    {"better": dummy_data.y_std, "worse": dummy_data.y_std + eps},
+)
+pmv.io.save_and_compress_svg(ax, "error-decay-with-uncert-multiple")
--- pymatviz/utils.py
+++ pymatviz/utils.py
@@ -729,3 +729,18 @@ def normalize_to_dict(
     raise TypeError(
         f"Invalid {inputs=}, expected {cls_name} or dict/list/tuple of {cls_name}"
     )
+
+
+def apply_matplotlib_template() -> None:
+    """Set default matplotlib configurations for consistency.
+    - Font size: 14 for readability.
+    - Savefig: Tight bounding box and 200 DPI for high-quality saved plots.
+    - Axes: Title size 16, bold weight for emphasis.
+    - Figure: DPI 200, title size 20, bold weight for better visibility.
+    - Layout: Enables constrained layout to reduce element overlap.
+    """
+    plt.rc("font", size=14)
+    plt.rc("savefig", bbox="tight", dpi=200)
+    plt.rc("axes", titlesize=16, titleweight="bold")
+    plt.rc("figure", dpi=200, titlesize=20, titleweight="bold")
+    plt.rcParams["figure.constrained_layout.use"] = True
