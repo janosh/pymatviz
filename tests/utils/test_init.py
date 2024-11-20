fcad156c052719629b89160d103cd522c5337438
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-11-20 11:08:09 +0800

    also check PKG_DIR

--- tests/utils/test_init.py
+++ tests/utils/test_init.py
@@ -5,8 +5,9 @@ import os
 from pymatviz.utils import PKG_DIR, ROOT
 
 
-assert os.path.isdir(ROOT)
 assert os.path.isdir(PKG_DIR)
+assert os.path.isdir(ROOT)
 
 
+assert os.path.dirname(PKG_DIR) == ROOT
 assert set(os.listdir(ROOT)).issuperset({"examples", "pymatviz", "tests", "assets"})
