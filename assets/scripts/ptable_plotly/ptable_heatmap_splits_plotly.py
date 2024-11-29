9e42b714f4b41d40ddaf17f3d1fa26f2e0a9d885
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-11-29 12:12:39 -0500

    add ptable_heatmap_splits_plotly example with callable colorscale to compare element color schemes
    add ptable-heatmap-splits-plotly-(2|3)-color-schemes.svg

--- assets/scripts/ptable_plotly/ptable_heatmap_splits_plotly.py
+++ assets/scripts/ptable_plotly/ptable_heatmap_splits_plotly.py
@@ -1,10 +1,13 @@
 # %%
 import itertools
+from collections.abc import Callable, Sequence
 
 import numpy as np
-from pymatgen.core.periodic_table import Element
+from pymatgen.core import Element
 
 import pymatviz as pmv
+import pymatviz.colors as pmv_colors
+from pymatviz.typing import RgbColorType
 
 
 np_rng = np.random.default_rng(seed=0)
@@ -34,3 +37,55 @@ for n_splits, orientation in itertools.product(
     fig.show()
     if orientation == "diagonal":
         pmv.io.save_and_compress_svg(fig, f"ptable-heatmap-splits-plotly-{n_splits}")
+
+
+# %% Visualize multiple element color schemes on a split periodic table heatmap
+def make_color_scale(
+    color_schemes: Sequence[dict[str, RgbColorType]],
+) -> Callable[[str, float, int], str]:
+    """Return element colors in different palettes based on split index."""
+
+    def elem_color_scale(element: str, _val: float, split_idx: int) -> str:
+        color = color_schemes[split_idx].get(element)
+        if color is None:
+            raise ValueError(f"no color for {element=} in {split_idx=}")
+        return f"rgb{color}"
+
+    return elem_color_scale
+
+
+palettes_3 = (
+    pmv_colors.ELEM_COLORS_ALLOY,
+    pmv_colors.ELEM_COLORS_JMOL,
+    pmv_colors.ELEM_COLORS_VESTA,
+)
+
+fig = pmv.ptable_heatmap_splits_plotly(
+    # Use dummy values for all elements
+    {str(elem): list(range(len(palettes_3))) for elem in Element},
+    orientation="diagonal",  # could also use "grid"
+    colorscale=make_color_scale(palettes_3),
+    hover_data=dict.fromkeys(
+        map(str, Element), "top left: JMOL<br>top right: VESTA, bottom: ALLOY"
+    ),
+)
+title = (
+    "<b>Element color schemes</b><br>top left: JMOL, top right: VESTA, bottom: ALLOY"
+)
+fig.layout.title.update(text=title, x=0.4, y=0.8)
+fig.show()
+
+
+# %% Visualize multiple element color schemes on a split periodic table heatmap
+palettes_2 = (pmv_colors.ELEM_COLORS_ALLOY, pmv_colors.ELEM_COLORS_VESTA)
+
+fig = pmv.ptable_heatmap_splits_plotly(
+    # Use dummy values for all elements
+    {str(elem): list(range(len(palettes_2))) for elem in Element},
+    orientation="vertical",
+    colorscale=make_color_scale(palettes_2),
+    hover_data=dict.fromkeys(map(str, Element), "left: VESTA<br>right: ALLOY"),
+)
+title = "<b>Element color schemes</b><br>left: VESTA, right: ALLOY"
+fig.layout.title.update(text=title, x=0.4, y=0.8)
+fig.show()
