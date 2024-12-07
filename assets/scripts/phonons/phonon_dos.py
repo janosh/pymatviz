4c7cf8b2907e50274efe91b0765ee270d01da50f
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-12-07 17:02:31 -0500

    add phonopy yaml example to assets/scripts/phonons/phonon_dos.py

--- assets/scripts/phonons/phonon_dos.py
+++ assets/scripts/phonons/phonon_dos.py
@@ -11,13 +11,6 @@ from pymatviz.enums import Key
 from pymatviz.utils.testing import TEST_FILES
 
 
-# TODO: ffonons not working properly (see #195)
-try:
-    import ffonons  # noqa: F401
-except ImportError:
-    raise SystemExit(0) from None  # install ffonons to run this script
-
-
 # %% Plot phonon bands and DOS
 for mp_id, formula in (
     ("mp-2758", "Sr4Se4"),
@@ -25,7 +18,7 @@ for mp_id, formula in (
 ):
     docs = {}
     for path in glob(f"{TEST_FILES}/phonons/{mp_id}-{formula}-*.json.lzma"):
-        key = path.split("-")[-1].split(".")[0]
+        key = path.split("-", maxsplit=3)[-1].split(".")[0]
         with zopen(path) as file:
             docs[key] = json.loads(file.read(), cls=MontyDecoder)
 
@@ -36,4 +29,24 @@ for mp_id, formula in (
     fig = pmv.phonon_dos(ph_doses)
     fig.layout.title = dict(text=f"Phonon DOS of {formula} ({mp_id})", x=0.5, y=0.98)
     fig.layout.margin = dict(l=0, r=0, b=0, t=40)
-    pmv.io.save_and_compress_svg(fig, f"phonon-dos-{mp_id}")
+    fig.show()
+    # pmv.io.save_and_compress_svg(fig, f"phonon-dos-{mp_id}")
+
+
+# %% plot phonopy TotalDos
+try:
+    import phonopy
+except ImportError:
+    raise SystemExit(0) from None  # install phonopy to run this script
+
+phonopy_nacl = phonopy.load(
+    phonopy_yaml=f"{TEST_FILES}/phonons/NaCl/phonopy_disp.yaml.xz",
+    force_sets_filename=f"{TEST_FILES}/phonons/NaCl/force_sets.dat",
+)
+
+phonopy_nacl.run_mesh([10, 10, 10])
+phonopy_nacl.run_total_dos()
+phonopy_nacl.plot_total_dos()
+
+fig = pmv.phonon_dos(phonopy_nacl.total_dos)
+fig.show()
