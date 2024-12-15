b4a8011eb8a5f61d68cc1344a307aea0a391042c
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-12-15 13:21:14 +0800

    revert get CI env change

--- assets/scripts/structure_viz/structure_2d.py
+++ assets/scripts/structure_viz/structure_2d.py
@@ -37,7 +37,7 @@ os.makedirs(structure_dir, exist_ok=True)
 for mp_id in struct_mp_ids:
     struct_file = f"{structure_dir}/{mp_id}.json.gz"
     if not os.path.isfile(struct_file):
-        if os.getenv("CI") == "true":
+        if os.getenv("CI"):
             raise FileNotFoundError(
                 f"structure for {mp_id} not found, run this script locally to fetch it."
             )
