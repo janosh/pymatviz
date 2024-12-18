a8aa11c46be4632b75b6a947810b8e5525284610
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-12-18 16:17:09 +0800

    finish test

--- tests/utils/test_testing.py
+++ tests/utils/test_testing.py
@@ -12,10 +12,17 @@ def test_test_files_dir() -> None:
     assert os.path.isdir(TEST_FILES)
 
 
+@pytest.fixture
+def create_figure() -> tuple[plt.Figure, plt.Axes]:
+    """A matplotlib figure and axes for test."""
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.plot([1, 2, 3], [4, 5, 6])
+    return fig, ax
+
+
 class TestInteractiveCheck:
-    def test_good_figure(self) -> None:
-        fig, ax = plt.subplots()
-        ax.plot([1, 2, 3], [4, 5, 6])
+    def test_good_figure(self, create_figure: tuple[plt.Figure, plt.Axes]) -> None:
+        fig, ax = create_figure
 
         # Test Axes
         interactive_check(ax, elem_to_check="line")
@@ -26,15 +33,21 @@ class TestInteractiveCheck:
         # Test KeyboardInterrupt capture (should be treated as "y")
         interactive_check(fig, elem_to_check="line")
 
-    def test_bad_figure(self) -> None:
-        # TODO: xfail
-        fig, ax = plt.subplots()
-        ax.plot([1, 2, 3], [4, 5, 6])
+    @pytest.mark.xfail(reason="test bad figure")
+    def test_bad_figure(self, create_figure: tuple[plt.Figure, plt.Axes]) -> None:
+        fig, _ax = create_figure
 
         interactive_check(fig, elem_to_check="bad figure")
 
-    def test_skip_ci(self) -> None:
-        pass
+    def test_skip_ci(
+        self,
+        create_figure: tuple[plt.Figure, plt.Axes],
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        fig, _ax = create_figure
+
+        monkeypatch.setenv("CI", "true")
+        assert interactive_check(fig, elem_to_check="figure") is None
 
     def test_not_supported_type(self) -> None:
         plot = [1, 2, 3]
